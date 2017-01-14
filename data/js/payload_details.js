9bad6f5fe4306e0930203b6636abeb80a73d8837
Steven <steven.hill1@gmail.com>
2017-01-14 01:10:14 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4995,
-	"created_at": "Fri Jan 13 01:10:18 +0000 2017",
+	"tweets": 4996,
+	"created_at": "Sat Jan 14 01:10:13 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_01.js
+++ data/js/tweets/2017_01.js
@@ -1,6 +1,329 @@
 Grailbird.data.tweets_2017_01 = 
 [
 	{
+		"created_at": "Fri Jan 13 18:15:23 +0000 2017",
+		"id": 819971118463385600,
+		"id_str": "819971118463385605",
+		"text": "RT @profsandyoliver: So exciting: 1st issue of Research for All will be out by the end of the month. And many more great contributions in f…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "profsandyoliver",
+					"name": "Sandy Oliver",
+					"id": 172361526,
+					"id_str": "172361526",
+					"indices": [
+						3,
+						19
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Head of Research Policy at the Higher Education Funding Council for England. Interested in research policy, science, evidence and innovation.",
+			"url": "http://t.co/m7YtMKvHbF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/m7YtMKvHbF",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3466,
+			"friends_count": 915,
+			"listed_count": 183,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 10,
+			"utc_offset": 0,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4996,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Jan 13 09:49:19 +0000 2017",
+			"id": 819843762042130400,
+			"id_str": "819843762042130432",
+			"text": "So exciting: 1st issue of Research for All will be out by the end of the month. And many more great contributions i… https://t.co/yOTRXTnnCd",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/yOTRXTnnCd",
+						"expanded_url": "https://twitter.com/i/web/status/819843762042130432",
+						"display_url": "twitter.com/i/web/status/8…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 172361526,
+				"id_str": "172361526",
+				"name": "Sandy Oliver",
+				"screen_name": "profsandyoliver",
+				"location": "London",
+				"description": "Making knowledge more democratic through inclusive approaches to doing and using research, especially systematic reviews. \r\nViews expressed are my own.",
+				"url": "http://t.co/2FkOKxa9zU",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/2FkOKxa9zU",
+								"expanded_url": "http://www.ioe.ac.uk/research/SSRU_38.html",
+								"display_url": "ioe.ac.uk/research/SSRU_…",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1732,
+				"friends_count": 913,
+				"listed_count": 69,
+				"created_at": "Thu Jul 29 14:39:02 +0000 2010",
+				"favourites_count": 921,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5566,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/761254390/4d8f133fd31f096fdc71106993afc346.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/761254390/4d8f133fd31f096fdc71106993afc346.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/742816226389458944/nsENzfsH_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/742816226389458944/nsENzfsH_normal.jpg",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 819823804499316700,
+			"quoted_status_id_str": "819823804499316736",
+			"quoted_status": {
+				"created_at": "Fri Jan 13 08:30:01 +0000 2017",
+				"id": 819823804499316700,
+				"id_str": "819823804499316736",
+				"text": "Research for All - 1st issue out end of January! https://t.co/Ge0hBvZXcw Browse here: https://t.co/AVpQlrxfOQ… https://t.co/Zl9S7GwPV3",
+				"truncated": true,
+				"entities": {
+					"hashtags": [],
+					"symbols": [],
+					"user_mentions": [],
+					"urls": [
+						{
+							"url": "https://t.co/Ge0hBvZXcw",
+							"expanded_url": "http://bit.ly/1OKMnMb",
+							"display_url": "bit.ly/1OKMnMb",
+							"indices": [
+								49,
+								72
+							]
+						},
+						{
+							"url": "https://t.co/AVpQlrxfOQ",
+							"expanded_url": "http://bit.ly/2j6J0Jw",
+							"display_url": "bit.ly/2j6J0Jw",
+							"indices": [
+								86,
+								109
+							]
+						},
+						{
+							"url": "https://t.co/Zl9S7GwPV3",
+							"expanded_url": "https://twitter.com/i/web/status/819823804499316736",
+							"display_url": "twitter.com/i/web/status/8…",
+							"indices": [
+								111,
+								134
+							]
+						}
+					]
+				},
+				"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+				"in_reply_to_status_id": null,
+				"in_reply_to_status_id_str": null,
+				"in_reply_to_user_id": null,
+				"in_reply_to_user_id_str": null,
+				"in_reply_to_screen_name": null,
+				"user": {
+					"id": 1710297276,
+					"id_str": "1710297276",
+					"name": "UCL IOE Press",
+					"screen_name": "IOE_Press",
+					"location": "London",
+					"description": "UCL IOE Press and Trentham Books publish leading research in education and social science. Based at the UCL Institute of Education. @IOE_London @ucl",
+					"url": "https://t.co/sE8rPQOP7J",
+					"entities": {
+						"url": {
+							"urls": [
+								{
+									"url": "https://t.co/sE8rPQOP7J",
+									"expanded_url": "http://www.ucl-ioe-press.com",
+									"display_url": "ucl-ioe-press.com",
+									"indices": [
+										0,
+										23
+									]
+								}
+							]
+						},
+						"description": {
+							"urls": []
+						}
+					},
+					"protected": false,
+					"followers_count": 1397,
+					"friends_count": 1133,
+					"listed_count": 67,
+					"created_at": "Thu Aug 29 15:37:19 +0000 2013",
+					"favourites_count": 268,
+					"utc_offset": -39600,
+					"time_zone": "International Date Line West",
+					"geo_enabled": true,
+					"verified": false,
+					"statuses_count": 2810,
+					"lang": "en",
+					"contributors_enabled": false,
+					"is_translator": false,
+					"is_translation_enabled": false,
+					"profile_background_color": "000000",
+					"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+					"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+					"profile_background_tile": false,
+					"profile_image_url": "http://pbs.twimg.com/profile_images/702154698837663745/oUgmjApg_normal.png",
+					"profile_image_url_https": "https://pbs.twimg.com/profile_images/702154698837663745/oUgmjApg_normal.png",
+					"profile_banner_url": "https://pbs.twimg.com/profile_banners/1710297276/1456242164",
+					"profile_link_color": "AC145A",
+					"profile_sidebar_border_color": "000000",
+					"profile_sidebar_fill_color": "000000",
+					"profile_text_color": "000000",
+					"profile_use_background_image": false,
+					"has_extended_profile": false,
+					"default_profile": false,
+					"default_profile_image": false,
+					"following": false,
+					"follow_request_sent": false,
+					"notifications": false,
+					"translator_type": "none"
+				},
+				"geo": null,
+				"coordinates": null,
+				"place": null,
+				"contributors": null,
+				"is_quote_status": false,
+				"retweet_count": 4,
+				"favorite_count": 8,
+				"favorited": false,
+				"retweeted": false,
+				"possibly_sensitive": false,
+				"lang": "en"
+			},
+			"retweet_count": 12,
+			"favorite_count": 13,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": true,
+		"quoted_status_id": 819823804499316700,
+		"quoted_status_id_str": "819823804499316736",
+		"retweet_count": 12,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Jan 12 10:00:17 +0000 2017",
 		"id": 819484132355043300,
 		"id_str": "819484132355043328",
