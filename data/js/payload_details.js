e2d5db447ff51e4b4987f80d4f06c75a75591e36
Steven <steven.hill1@gmail.com>
2018-01-19 01:10:14 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5513,
-	"created_at": "Thu Jan 18 01:10:14 +0000 2018",
+	"tweets": 5514,
+	"created_at": "Fri Jan 19 01:10:13 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_01.js
+++ data/js/tweets/2018_01.js
@@ -1,6 +1,176 @@
 Grailbird.data.tweets_2018_01 = 
 [
 	{
+		"created_at": "Thu Jan 18 09:34:46 +0000 2018",
+		"id": 953923610149838800,
+		"id_str": "953923610149838849",
+		"text": "@BillCookeIII @DSweeneyHEFCE @JennyRohn @MikeTaylor @CharlesOppenh @HEFCE @DORAssessment We want to discuss this wi… https://t.co/GxCeyTKAha",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BillCookeIII",
+					"name": "Bill M Cooke",
+					"id": 593025856,
+					"id_str": "593025856",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "DSweeneyHEFCE",
+					"name": "David Sweeney",
+					"id": 4162904121,
+					"id_str": "4162904121",
+					"indices": [
+						14,
+						28
+					]
+				},
+				{
+					"screen_name": "JennyRohn",
+					"name": "Jennifer L. Rohn",
+					"id": 135583203,
+					"id_str": "135583203",
+					"indices": [
+						29,
+						39
+					]
+				},
+				{
+					"screen_name": "MikeTaylor",
+					"name": "Mike Tⓐylor",
+					"id": 278930750,
+					"id_str": "278930750",
+					"indices": [
+						40,
+						51
+					]
+				},
+				{
+					"screen_name": "CharlesOppenh",
+					"name": "Charles Oppenheim",
+					"id": 259625864,
+					"id_str": "259625864",
+					"indices": [
+						52,
+						66
+					]
+				},
+				{
+					"screen_name": "HEFCE",
+					"name": "HEFCE",
+					"id": 20054581,
+					"id_str": "20054581",
+					"indices": [
+						67,
+						73
+					]
+				},
+				{
+					"screen_name": "DORAssessment",
+					"name": "DORA",
+					"id": 3201131333,
+					"id_str": "3201131333",
+					"indices": [
+						74,
+						88
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/GxCeyTKAha",
+					"expanded_url": "https://twitter.com/i/web/status/953923610149838849",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 953745028966420500,
+		"in_reply_to_status_id_str": "953745028966420481",
+		"in_reply_to_user_id": 593025856,
+		"in_reply_to_user_id_str": "593025856",
+		"in_reply_to_screen_name": "BillCookeIII",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Head of Research Policy at the Higher Education Funding Council for England. Interested in research policy, science, evidence and innovation.",
+			"url": "http://t.co/m7YtMKvHbF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/m7YtMKvHbF",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3920,
+			"friends_count": 956,
+			"listed_count": 187,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 41,
+			"utc_offset": 0,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5512,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Jan 17 19:32:04 +0000 2018",
 		"id": 953711539818819600,
 		"id_str": "953711539818819584",
