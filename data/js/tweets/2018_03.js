b93b3077b90c0b8d6110bfdb920eaa4bbfedffb4
Steven <steven.hill1@gmail.com>
2018-03-21 01:10:14 +0000

    Added 1 tweets

--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Tue Mar 20 19:07:24 +0000 2018",
+		"id": 976173378284466200,
+		"id_str": "976173378284466176",
+		"text": "@cemathieson @magistraetmater @burnyourbones @pennyb @DrBenLitherland Got to this late, but yes, there is an OA exc… https://t.co/rP0ukVsNRV",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cemathieson",
+					"name": "Dr Charlotte Mathieson",
+					"id": 385021848,
+					"id_str": "385021848",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "magistraetmater",
+					"name": "Magistra et Mater",
+					"id": 155517617,
+					"id_str": "155517617",
+					"indices": [
+						13,
+						29
+					]
+				},
+				{
+					"screen_name": "burnyourbones",
+					"name": "carrier bag tree ghost",
+					"id": 14636827,
+					"id_str": "14636827",
+					"indices": [
+						30,
+						44
+					]
+				},
+				{
+					"screen_name": "pennyb",
+					"name": "Penny CS Andrews👨🏻‍🎤🌈🔥",
+					"id": 6069772,
+					"id_str": "6069772",
+					"indices": [
+						45,
+						52
+					]
+				},
+				{
+					"screen_name": "DrBenLitherland",
+					"name": "Benjamin Litherland",
+					"id": 742123579375005700,
+					"id_str": "742123579375005696",
+					"indices": [
+						53,
+						69
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/rP0ukVsNRV",
+					"expanded_url": "https://twitter.com/i/web/status/976173378284466176",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 976108213098745900,
+		"in_reply_to_status_id_str": "976108213098745857",
+		"in_reply_to_user_id": 385021848,
+		"in_reply_to_user_id_str": "385021848",
+		"in_reply_to_screen_name": "cemathieson",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Head of Research Policy at the Higher Education Funding Council for England. Interested in research policy, science, evidence and innovation.",
+			"url": "http://t.co/m7YtMKvHbF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/m7YtMKvHbF",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4002,
+			"friends_count": 962,
+			"listed_count": 186,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 50,
+			"utc_offset": 0,
+			"time_zone": "London",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5558,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Mar 17 09:00:06 +0000 2018",
 		"id": 974933384735666200,
 		"id_str": "974933384735666176",
