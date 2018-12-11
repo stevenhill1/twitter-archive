360579c4e0bc0ef6f76c60f7740ba47eb817287d
Steven <steven.hill1@gmail.com>
2018-12-11 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5797,
-	"created_at": "Sat Dec 01 01:10:13 +0000 2018",
+	"tweets": 5798,
+	"created_at": "Tue Dec 11 01:10:12 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_12.js
@@ -0,0 +1,172 @@
+Grailbird.data.tweets_2018_12 = 
+[
+	{
+		"created_at": "Mon Dec 10 08:51:22 +0000 2018",
+		"id": 1072051130962731000,
+		"id_str": "1072051130962731009",
+		"full_text": "How can societal #impact be measured and evaluated? @MilSaari blogs about lessons learned from #REF impact assessments by @GemmaDerrick and @stevenhill. These experiences are useful as we at @Akatemia_STN plan the #research programme evaluation concept https://t.co/wYGDHAW8tC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "impact",
+					"indices": [
+						17,
+						24
+					]
+				},
+				{
+					"text": "REF",
+					"indices": [
+						95,
+						99
+					]
+				},
+				{
+					"text": "research",
+					"indices": [
+						214,
+						223
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MilSaari",
+					"name": "Milja Saari",
+					"id": 1111311990,
+					"id_str": "1111311990",
+					"indices": [
+						52,
+						61
+					]
+				},
+				{
+					"screen_name": "GemmaDerrick",
+					"name": "Gemma E Derrick",
+					"id": 475031908,
+					"id_str": "475031908",
+					"indices": [
+						122,
+						135
+					]
+				},
+				{
+					"screen_name": "stevenhill",
+					"name": "Steven Hill",
+					"id": 7731752,
+					"id_str": "7731752",
+					"indices": [
+						140,
+						151
+					]
+				},
+				{
+					"screen_name": "Akatemia_STN",
+					"name": "Strateginen tutkimus",
+					"id": 4060455820,
+					"id_str": "4060455820",
+					"indices": [
+						191,
+						204
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/wYGDHAW8tC",
+					"expanded_url": "https://bit.ly/2UvmUkj",
+					"display_url": "bit.ly/2UvmUkj",
+					"indices": [
+						253,
+						276
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4060455820,
+			"id_str": "4060455820",
+			"name": "Strateginen tutkimus",
+			"screen_name": "Akatemia_STN",
+			"location": "",
+			"description": "Rahoitamme yhteiskunnallisesti merkittävää ja vaikuttavaa korkeatasoista tiedettä. STN toimii @SuomenAkatemia'n yhteydessä. #strateginentutkimus",
+			"url": "https://t.co/Diii0UcEQ5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Diii0UcEQ5",
+							"expanded_url": "http://www.aka.fi/stn",
+							"display_url": "aka.fi/stn",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2599,
+			"friends_count": 579,
+			"listed_count": 27,
+			"created_at": "Wed Oct 28 11:55:33 +0000 2015",
+			"favourites_count": 2301,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2306,
+			"lang": "fi",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/666978588072243202/1yyYlPaT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/666978588072243202/1yyYlPaT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4060455820/1447850578",
+			"profile_link_color": "95C11F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "How can societal #impact be measured and evaluated? @MilSaari blogs about lessons learned from #REF impact assessments by @GemmaDerrick and @stevenhill. These experiences are useful as we at @Akatemia_STN plan the #research programme evaluation concept https://t.co/wYGDHAW8tC"
+	}
+]
