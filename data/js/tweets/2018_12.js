73ffafc5eed3a67a37a73b43cc3cdf5e29bc3561
Steven <steven.hill1@gmail.com>
2018-12-13 01:10:13 +0000

    Added 1 tweets

--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,271 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Thu Dec 06 11:09:20 +0000 2018",
+		"id": 1070636301345546200,
+		"id_str": "1070636301345546240",
+		"full_text": "Towards a #PlanS gap analysis? #openaccess potential (journals and publications) across disciplines. First approach using Web of Science, also discussing limitations and biases resulting from that. \nhttps://t.co/qHyc6o0J04\nhttps://t.co/5plbRprvv7 w/ @jeroenbosman https://t.co/o9Mjd8gR6F",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			263
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PlanS",
+					"indices": [
+						10,
+						16
+					]
+				},
+				{
+					"text": "openaccess",
+					"indices": [
+						31,
+						42
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jeroenbosman",
+					"name": "Jeroen Bosman",
+					"id": 9104202,
+					"id_str": "9104202",
+					"indices": [
+						250,
+						263
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qHyc6o0J04",
+					"expanded_url": "https://101innovations.wordpress.com/2018/12/05/oa-potential-journals-and-publications-across-disciplines/",
+					"display_url": "101innovations.wordpress.com/2018/12/05/oa-…",
+					"indices": [
+						199,
+						222
+					]
+				},
+				{
+					"url": "https://t.co/5plbRprvv7",
+					"expanded_url": "https://101innovations.wordpress.com/2018/12/05/gold-oa-journals-in-wos-and-doaj/",
+					"display_url": "101innovations.wordpress.com/2018/12/05/gol…",
+					"indices": [
+						223,
+						246
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1070606786720383000,
+					"id_str": "1070606786720382976",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/DtuPM0CXQAAjQuq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtuPM0CXQAAjQuq.jpg",
+					"url": "https://t.co/o9Mjd8gR6F",
+					"display_url": "pic.twitter.com/o9Mjd8gR6F",
+					"expanded_url": "https://twitter.com/MsPhelps/status/1070636301345546240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 396,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1462,
+							"h": 482,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 224,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1070606786720383000,
+					"id_str": "1070606786720382976",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/DtuPM0CXQAAjQuq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtuPM0CXQAAjQuq.jpg",
+					"url": "https://t.co/o9Mjd8gR6F",
+					"display_url": "pic.twitter.com/o9Mjd8gR6F",
+					"expanded_url": "https://twitter.com/MsPhelps/status/1070636301345546240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 396,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1462,
+							"h": 482,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 224,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1070607399843782700,
+					"id_str": "1070607399843782657",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/DtuPwgGX4AEuuiv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtuPwgGX4AEuuiv.jpg",
+					"url": "https://t.co/o9Mjd8gR6F",
+					"display_url": "pic.twitter.com/o9Mjd8gR6F",
+					"expanded_url": "https://twitter.com/MsPhelps/status/1070636301345546240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 22963112,
+			"id_str": "22963112",
+			"name": "Biⓐnca Kramer",
+			"screen_name": "MsPhelps",
+			"location": "Utrecht, The Netherlands",
+			"description": "Utrecht University Library, 101 Innovations in Scholarly Communication, Open science , @force11rescomm board https://t.co/VZxgPZqN4x",
+			"url": "https://t.co/IxfosTswir",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IxfosTswir",
+							"expanded_url": "http://101innovations.wordpress.com",
+							"display_url": "101innovations.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VZxgPZqN4x",
+							"expanded_url": "http://orcid.org/0000-0002-5965-6560",
+							"display_url": "orcid.org/0000-0002-5965…",
+							"indices": [
+								109,
+								132
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3383,
+			"friends_count": 1636,
+			"listed_count": 175,
+			"created_at": "Thu Mar 05 19:20:50 +0000 2009",
+			"favourites_count": 9045,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 12146,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1031464996801323008/gBJoV05c_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1031464996801323008/gBJoV05c_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/22963112/1457476751",
+			"profile_link_color": "BF0000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 25,
+		"favorite_count": 25,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Towards a #PlanS gap analysis? #openaccess potential (journals and publications) across disciplines. First approach using Web of Science, also discussing limitations and biases resulting from that. \nhttps://t.co/qHyc6o0J04\nhttps://t.co/5plbRprvv7 w/ @jeroenbosman https://t.co/o9Mjd8gR6F"
+	},
+	{
 		"created_at": "Tue Dec 11 18:11:38 +0000 2018",
 		"id": 1072554515025616900,
 		"id_str": "1072554515025616896",
