dcd34af480310c1a5e794601f95bb3927a27aa37
Steven <steven.hill1@gmail.com>
2019-01-04 01:10:13 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5838,
-	"created_at": "Thu Jan 03 01:10:13 +0000 2019",
+	"tweets": 5840,
+	"created_at": "Fri Jan 04 01:10:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Thu Jan 03 16:01:19 +0000 2019",
+		"id": 1080856643162816500,
+		"id_str": "1080856643162816512",
+		"full_text": "@UofGVC @DavidPriceUCL @GraemeTR I recall @DavidPriceUCL &amp; I wrote an open letter to Vince Cable in 2010 suggesting an increase in block grants to further improve the efficiency of allocation. QR is efficient &amp; effective way to distribute public funds for research. REF provides longer term feedback loop.",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			313
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UofGVC",
+					"name": "Anton Muscatelli",
+					"id": 874563713264693200,
+					"id_str": "874563713264693248",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "DavidPriceUCL",
+					"name": "David Price",
+					"id": 1478149944,
+					"id_str": "1478149944",
+					"indices": [
+						8,
+						22
+					]
+				},
+				{
+					"screen_name": "GraemeTR",
+					"name": "Graeme Reid",
+					"id": 60871529,
+					"id_str": "60871529",
+					"indices": [
+						23,
+						32
+					]
+				},
+				{
+					"screen_name": "DavidPriceUCL",
+					"name": "David Price",
+					"id": 1478149944,
+					"id_str": "1478149944",
+					"indices": [
+						42,
+						56
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1080836515117314000,
+		"in_reply_to_status_id_str": "1080836515117314049",
+		"in_reply_to_user_id": 874563713264693200,
+		"in_reply_to_user_id_str": "874563713264693248",
+		"in_reply_to_screen_name": "UofGVC",
+		"user": {
+			"id": 848098054473478100,
+			"id_str": "848098054473478145",
+			"name": "Stephen Caddick",
+			"screen_name": "Stephen_Caddick",
+			"location": "London, England",
+			"description": "Innovation Director @WellcomeTrust & UCL Professor of Chemistry. \nTweeting in a personal capacity",
+			"url": "https://t.co/XdzehMwfrj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XdzehMwfrj",
+							"expanded_url": "http://www.researcherid.com/rid/B-8550-2009",
+							"display_url": "researcherid.com/rid/B-8550-2009",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 360,
+			"friends_count": 56,
+			"listed_count": 6,
+			"created_at": "Sat Apr 01 09:01:47 +0000 2017",
+			"favourites_count": 79,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 502,
+			"lang": "en-gb",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/863082198492221442/70Ei8JzW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/863082198492221442/70Ei8JzW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/848098054473478145/1499013632",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@UofGVC @DavidPriceUCL @GraemeTR I recall @DavidPriceUCL &amp; I wrote an open letter to Vince Cable in 2010 suggesting an increase in block grants to further improve the efficiency of allocation. QR is efficient &amp; effective way to distribute public funds for research. REF provides longer term feedback loop."
+	},
+	{
+		"created_at": "Wed Jan 02 10:07:09 +0000 2019",
+		"id": 1080405124479180800,
+		"id_str": "1080405124479180800",
+		"full_text": "Do you want to use the @BritishSciFest as a platform for your idea, research or artistic endeavour? Apply now through our open call and join us in Coventry &amp; Warwickshire this September! https://t.co/NhmvwNQQko",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			214
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BritishSciFest",
+					"name": "British Science Fest",
+					"id": 16434273,
+					"id_str": "16434273",
+					"indices": [
+						23,
+						38
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NhmvwNQQko",
+					"expanded_url": "https://www.britishscienceassociation.org/british-science-festival",
+					"display_url": "britishscienceassociation.org/british-scienc…",
+					"indices": [
+						191,
+						214
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 53360996,
+			"id_str": "53360996",
+			"name": "Ivvet Modinou",
+			"screen_name": "IvvetM",
+			"location": "London born, Cypriot bred",
+			"description": "Film, music, science/tech, politics & the odd kitten. Head of Engagement @BritSciAssoc & Director @BritishSciFest; Creator @thesaturdaysch1;co-founder @richseam",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2252,
+			"friends_count": 1965,
+			"listed_count": 75,
+			"created_at": "Fri Jul 03 11:29:18 +0000 2009",
+			"favourites_count": 5936,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5721,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C6E2EE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000245569768/1788db3fddc062d2655bf9502f989fbf_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000245569768/1788db3fddc062d2655bf9502f989fbf_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/53360996/1433467634",
+			"profile_link_color": "1F98C7",
+			"profile_sidebar_border_color": "C6E2EE",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 21,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Do you want to use the @BritishSciFest as a platform for your idea, research or artistic endeavour? Apply now through our open call and join us in Coventry &amp; Warwickshire this September! https://t.co/NhmvwNQQko"
+	},
+	{
 		"created_at": "Wed Jan 02 10:07:09 +0000 2019",
 		"id": 1080405124479180800,
 		"id_str": "1080405124479180800",
