ac03534e009ff71652c33fc2028cf16e07038555
Steven <steven.hill1@gmail.com>
2019-01-05 01:10:13 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5840,
-	"created_at": "Fri Jan 04 01:10:12 +0000 2019",
+	"tweets": 5842,
+	"created_at": "Sat Jan 05 01:10:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Fri Jan 04 18:37:39 +0000 2019",
+		"id": 1081258374455615500,
+		"id_str": "1081258374455615488",
+		"full_text": "A real pleasure to discuss all things @ResEngland with @CSkidmoreUK this afternoon https://t.co/5GgR34vROG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						38,
+						49
+					]
+				},
+				{
+					"screen_name": "CSkidmoreUK",
+					"name": "Chris Skidmore",
+					"id": 436576452,
+					"id_str": "436576452",
+					"indices": [
+						55,
+						67
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5GgR34vROG",
+					"expanded_url": "https://twitter.com/ResEngland/status/1081220517196152832",
+					"display_url": "twitter.com/ResEngland/sta…",
+					"indices": [
+						83,
+						106
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4419,
+			"friends_count": 979,
+			"listed_count": 181,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 68,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5777,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A real pleasure to discuss all things @ResEngland with @CSkidmoreUK this afternoon https://t.co/5GgR34vROG"
+	},
+	{
+		"created_at": "Thu Jan 03 16:01:19 +0000 2019",
+		"id": 1080856643162816500,
+		"id_str": "1080856643162816512",
+		"full_text": "@UofGVC @DavidPriceUCL @GraemeTR I recall @DavidPriceUCL &amp; I wrote an open letter to Vince Cable in 2010 suggesting an increase in block grants to further improve the efficiency of allocation. QR is efficient &amp; effective way to distribute public funds for research. REF provides longer term feedback loop.",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			313
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UofGVC",
+					"name": "Anton Muscatelli",
+					"id": 874563713264693200,
+					"id_str": "874563713264693248",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "DavidPriceUCL",
+					"name": "David Price",
+					"id": 1478149944,
+					"id_str": "1478149944",
+					"indices": [
+						8,
+						22
+					]
+				},
+				{
+					"screen_name": "GraemeTR",
+					"name": "Graeme Reid",
+					"id": 60871529,
+					"id_str": "60871529",
+					"indices": [
+						23,
+						32
+					]
+				},
+				{
+					"screen_name": "DavidPriceUCL",
+					"name": "David Price",
+					"id": 1478149944,
+					"id_str": "1478149944",
+					"indices": [
+						42,
+						56
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1080836515117314000,
+		"in_reply_to_status_id_str": "1080836515117314049",
+		"in_reply_to_user_id": 874563713264693200,
+		"in_reply_to_user_id_str": "874563713264693248",
+		"in_reply_to_screen_name": "UofGVC",
+		"user": {
+			"id": 848098054473478100,
+			"id_str": "848098054473478145",
+			"name": "Stephen Caddick",
+			"screen_name": "Stephen_Caddick",
+			"location": "London, England",
+			"description": "Innovation Director @WellcomeTrust & UCL Professor of Chemistry. \nTweeting in a personal capacity",
+			"url": "https://t.co/XdzehMwfrj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XdzehMwfrj",
+							"expanded_url": "http://www.researcherid.com/rid/B-8550-2009",
+							"display_url": "researcherid.com/rid/B-8550-2009",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 363,
+			"friends_count": 56,
+			"listed_count": 6,
+			"created_at": "Sat Apr 01 09:01:47 +0000 2017",
+			"favourites_count": 79,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 502,
+			"lang": "en-gb",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/863082198492221442/70Ei8JzW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/863082198492221442/70Ei8JzW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/848098054473478145/1499013632",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 7,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@UofGVC @DavidPriceUCL @GraemeTR I recall @DavidPriceUCL &amp; I wrote an open letter to Vince Cable in 2010 suggesting an increase in block grants to further improve the efficiency of allocation. QR is efficient &amp; effective way to distribute public funds for research. REF provides longer term feedback loop."
+	},
+	{
 		"created_at": "Thu Jan 03 16:01:19 +0000 2019",
 		"id": 1080856643162816500,
 		"id_str": "1080856643162816512",
