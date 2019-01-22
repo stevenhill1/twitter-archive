8f34278a52fb51c55ad4d67fa5b2b5ca38b2ab36
Steven <steven.hill1@gmail.com>
2019-01-22 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5851,
-	"created_at": "Thu Jan 17 01:10:12 +0000 2019",
+	"tweets": 5852,
+	"created_at": "Tue Jan 22 01:10:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Mon Jan 21 12:05:05 +0000 2019",
+		"id": 1087320172157960200,
+		"id_str": "1087426590483644416",
+		"full_text": "We're hugely excited to welcome @gmabbam to the BSA family as our new Chair 🥳 She will bring a wealth of passion and experience over the coming years to help us achieve our vision. Read more about her and her amazing achievements here 👇 https://t.co/YcLGx03cF9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gmabbam",
+					"name": "Gisela Abbam",
+					"id": 69254763,
+					"id_str": "69254763",
+					"indices": [
+						32,
+						40
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/YcLGx03cF9",
+					"expanded_url": "https://www.britishscienceassociation.org/news/gisela-abbam-appointed-chair-of-british-science-association",
+					"display_url": "britishscienceassociation.org/news/gisela-ab…",
+					"indices": [
+						237,
+						260
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 442847698,
+			"id_str": "442847698",
+			"name": "British Science Association",
+			"screen_name": "BritSciAssoc",
+			"location": "London",
+			"description": "Our vision is a world where science is seen as a fundamental part of our culture and society.",
+			"url": "http://t.co/xffbzLXKst",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/xffbzLXKst",
+							"expanded_url": "http://www.britishscienceassociation.org",
+							"display_url": "britishscienceassociation.org",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 46778,
+			"friends_count": 3008,
+			"listed_count": 596,
+			"created_at": "Wed Dec 21 14:49:07 +0000 2011",
+			"favourites_count": 14117,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11742,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "111111",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/879263136297226240/2rseI9Qh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/879263136297226240/2rseI9Qh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/442847698/1498467665",
+			"profile_link_color": "25A09A",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We're hugely excited to welcome @gmabbam to the BSA family as our new Chair 🥳 She will bring a wealth of passion and experience over the coming years to help us achieve our vision. Read more about her and her amazing achievements here 👇 https://t.co/YcLGx03cF9"
+	},
+	{
 		"created_at": "Wed Jan 16 14:12:03 +0000 2019",
 		"id": 1085540185654349800,
 		"id_str": "1085540185654349825",
