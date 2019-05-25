15a259cbc51cb7eaa9328feee98316a2369a2383
Steven <steven.hill1@gmail.com>
2019-05-25 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5861,
-	"created_at": "Tue May 21 01:10:13 +0000 2019",
+	"tweets": 5862,
+	"created_at": "Sat May 25 01:10:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,10 +1,126 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 24 17:12:09 +0000 2019",
+		"id": 1131971155609497600,
+		"id_str": "1131971155609497601",
+		"full_text": "The @royalsociety has published a piece I wrote for them a little while ago, alert their research culture conference in October. \"Time for more research on research\" https://t.co/or6hM6bBiY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			189
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "royalsociety",
+					"name": "The Royal Society",
+					"id": 28567809,
+					"id_str": "28567809",
+					"indices": [
+						4,
+						17
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/or6hM6bBiY",
+					"expanded_url": "http://blogs.royalsociety.org/in-verba/2019/05/24/time-for-more-research-on-research/",
+					"display_url": "blogs.royalsociety.org/in-verba/2019/…",
+					"indices": [
+						166,
+						189
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://pragmaticcode.com/linky/\" rel=\"nofollow\">Linky for iOS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4549,
+			"friends_count": 979,
+			"listed_count": 181,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 69,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5786,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The @royalsociety has published a piece I wrote for them a little while ago, alert their research culture conference in October. \"Time for more research on research\" https://t.co/or6hM6bBiY"
+	},
+	{
 		"created_at": "Mon May 20 11:44:47 +0000 2019",
 		"id": 1130433523981201400,
 		"id_str": "1130439219850219521",
-		"full_text": "Have you ever wondered how the #KnowledgeExchange Framework impact element differs from the Research Excellence Framework (@REF_2021)? In this blog our Head of Research, @stevenhill and Head of Data &amp; Evidence, @hamish_mcalpine discuss the similarities and differences​. https://t.co/s7g0FLO5tu",
+		"full_text": "Have you ever wondered how the #KnowledgeExchange Framework impact element differs from the Research Excellence Framework (@REF_2021)? In this blog our Head of Research, @stevenhill and Head of Data &amp; Evidence, @hamish_mcalpine discuss the similarities and differences. https://t.co/s7g0FLO5tu",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -311,6 +427,6 @@ Grailbird.data.tweets_2019_05 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Have you ever wondered how the #KnowledgeExchange Framework impact element differs from the Research Excellence Framework (@REF_2021)? In this blog our Head of Research, @stevenhill and Head of Data &amp; Evidence, @hamish_mcalpine discuss the similarities and differences​. https://t.co/s7g0FLO5tu"
+		"text": "Have you ever wondered how the #KnowledgeExchange Framework impact element differs from the Research Excellence Framework (@REF_2021)? In this blog our Head of Research, @stevenhill and Head of Data &amp; Evidence, @hamish_mcalpine discuss the similarities and differences. https://t.co/s7g0FLO5tu"
 	}
 ]
