1868d7850b47fd4ae7b11b338f459b4f389b58cb
Steven <steven.hill1@gmail.com>
2019-06-08 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5863,
-	"created_at": "Fri May 31 01:10:12 +0000 2019",
+	"tweets": 5864,
+	"created_at": "Sat Jun 08 01:10:12 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_06.js
@@ -0,0 +1,129 @@
+Grailbird.data.tweets_2019_06 = 
+[
+	{
+		"created_at": "Fri Jun 07 20:20:02 +0000 2019",
+		"id": 1137091868536246300,
+		"id_str": "1137091868536246272",
+		"full_text": "@profmarkreed @BruceCronin I was referring to internal analysis at the level of submissions (UOA x institution) which we have never formally published (though its in many of my presentations). But the exact same analysis was done here: https://t.co/lEy28w2Cza",
+		"truncated": false,
+		"display_text_range": [
+			27,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "profmarkreed",
+					"name": "Mark Reed",
+					"id": 19347472,
+					"id_str": "19347472",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "BruceCronin",
+					"name": "Bruce Cronin",
+					"id": 2189981206,
+					"id_str": "2189981206",
+					"indices": [
+						14,
+						26
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/lEy28w2Cza",
+					"expanded_url": "https://doi.org/10.1371/journal.pone.0172817",
+					"display_url": "doi.org/10.1371/journa…",
+					"indices": [
+						236,
+						259
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": 1136761613313355800,
+		"in_reply_to_status_id_str": "1136761613313355777",
+		"in_reply_to_user_id": 19347472,
+		"in_reply_to_user_id_str": "19347472",
+		"in_reply_to_screen_name": "profmarkreed",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4561,
+			"friends_count": 979,
+			"listed_count": 181,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 70,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5788,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@profmarkreed @BruceCronin I was referring to internal analysis at the level of submissions (UOA x institution) which we have never formally published (though its in many of my presentations). But the exact same analysis was done here: https://t.co/lEy28w2Cza"
+	}
+]
