d4262b8f37d5e6e1776e88c591a8c591b031bb16
Steven <steven.hill1@gmail.com>
2019-07-03 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5864,
-	"created_at": "Sat Jun 08 01:10:12 +0000 2019",
+	"tweets": 5865,
+	"created_at": "Wed Jul 03 01:10:12 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_07.js
@@ -0,0 +1,152 @@
+Grailbird.data.tweets_2019_07 = 
+[
+	{
+		"created_at": "Tue Jul 02 11:29:17 +0000 2019",
+		"id": 1146017998802772000,
+		"id_str": "1146017998802771968",
+		"full_text": "Really please that the @ResEngland funding allocations are published today. Members of the Psychology, Psychiatry, and Neuroscience communities will be interested in para 14 of the circular letter: https://t.co/ckjUgmquQi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			221
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						23,
+						34
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ckjUgmquQi",
+					"expanded_url": "https://re.ukri.org/news-events-publications/publications/circular-letter-funding-for-higher-education-institutions-2019-20/",
+					"display_url": "re.ukri.org/news-events-pu…",
+					"indices": [
+						198,
+						221
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4583,
+			"friends_count": 977,
+			"listed_count": 180,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 70,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5789,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "1a3fd0b34e5cc504",
+			"url": "https://api.twitter.com/1.1/geo/id/1a3fd0b34e5cc504.json",
+			"place_type": "city",
+			"name": "Urmston",
+			"full_name": "Urmston, England",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-2.4094209,
+							53.4316145
+						],
+						[
+							-2.25304,
+							53.4316145
+						],
+						[
+							-2.25304,
+							53.480382
+						],
+						[
+							-2.4094209,
+							53.480382
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Really please that the @ResEngland funding allocations are published today. Members of the Psychology, Psychiatry, and Neuroscience communities will be interested in para 14 of the circular letter: https://t.co/ckjUgmquQi"
+	}
+]
