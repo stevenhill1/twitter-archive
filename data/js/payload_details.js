6be7de91fcc90f84347e221af5d13130ce7e7d6a
Steven <steven.hill1@gmail.com>
2019-07-07 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5865,
-	"created_at": "Wed Jul 03 01:10:12 +0000 2019",
+	"tweets": 5866,
+	"created_at": "Sun Jul 07 01:10:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,306 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sat Jul 06 15:49:06 +0000 2019",
+		"id": 1147410078489878500,
+		"id_str": "1147532935597363200",
+		"full_text": "Since 2012, the UK Research Partnership Investment Fund has enabled UK universities to develop state-of-the-art research facilities to support cutting-edge research and innovation in a range of specialist areas: https://t.co/fD2PgHWxgP\n\n #builtwithUKRPIF https://t.co/JCN3WxxR2H",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			254
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "builtwithUKRPIF",
+					"indices": [
+						238,
+						254
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fD2PgHWxgP",
+					"expanded_url": "https://bit.ly/2YCIxAQ",
+					"display_url": "bit.ly/2YCIxAQ",
+					"indices": [
+						212,
+						235
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1147410055530201100,
+					"id_str": "1147410055530201089",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/media/D-xrXcOWwAEy1_u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-xrXcOWwAEy1_u.jpg",
+					"url": "https://t.co/JCN3WxxR2H",
+					"display_url": "pic.twitter.com/JCN3WxxR2H",
+					"expanded_url": "https://twitter.com/ResEngland/status/1147410078489878528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 572,
+							"h": 858,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 572,
+							"h": 858,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1147410055530201100,
+					"id_str": "1147410055530201089",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/media/D-xrXcOWwAEy1_u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-xrXcOWwAEy1_u.jpg",
+					"url": "https://t.co/JCN3WxxR2H",
+					"display_url": "pic.twitter.com/JCN3WxxR2H",
+					"expanded_url": "https://twitter.com/ResEngland/status/1147410078489878528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 572,
+							"h": 858,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 572,
+							"h": 858,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1147410055584727000,
+					"id_str": "1147410055584727040",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/media/D-xrXcbWwAAYTO6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-xrXcbWwAAYTO6.jpg",
+					"url": "https://t.co/JCN3WxxR2H",
+					"display_url": "pic.twitter.com/JCN3WxxR2H",
+					"expanded_url": "https://twitter.com/ResEngland/status/1147410078489878528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 456,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1791,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 804,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1147410055526064100,
+					"id_str": "1147410055526064129",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/media/D-xrXcNXoAEjMCe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-xrXcNXoAEjMCe.jpg",
+					"url": "https://t.co/JCN3WxxR2H",
+					"display_url": "pic.twitter.com/JCN3WxxR2H",
+					"expanded_url": "https://twitter.com/ResEngland/status/1147410078489878528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1147410055526068200,
+					"id_str": "1147410055526068224",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/media/D-xrXcNXsAAiFNS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-xrXcNXsAAiFNS.jpg",
+					"url": "https://t.co/JCN3WxxR2H",
+					"display_url": "pic.twitter.com/JCN3WxxR2H",
+					"expanded_url": "https://twitter.com/ResEngland/status/1147410078489878528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 797,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 452,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1361,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 823853384138637300,
+			"id_str": "823853384138637313",
+			"name": "Research England",
+			"screen_name": "ResEngland",
+			"location": "Bristol",
+			"description": "We create and sustain the conditions for a healthy, dynamic research and knowledge exchange system in England's universities. Part of @UKRI_News",
+			"url": "https://t.co/BI90gIR8Ar",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BI90gIR8Ar",
+							"expanded_url": "https://re.ukri.org/",
+							"display_url": "re.ukri.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7224,
+			"friends_count": 359,
+			"listed_count": 85,
+			"created_at": "Tue Jan 24 11:22:08 +0000 2017",
+			"favourites_count": 1135,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1055,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1006515717888512000/KnXg55bl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1006515717888512000/KnXg55bl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/823853384138637313/1553858043",
+			"profile_link_color": "ACBD37",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Since 2012, the UK Research Partnership Investment Fund has enabled UK universities to develop state-of-the-art research facilities to support cutting-edge research and innovation in a range of specialist areas: https://t.co/fD2PgHWxgP\n\n #builtwithUKRPIF https://t.co/JCN3WxxR2H"
+	},
+	{
 		"created_at": "Tue Jul 02 11:29:17 +0000 2019",
 		"id": 1146017998802772000,
 		"id_str": "1146017998802771968",
