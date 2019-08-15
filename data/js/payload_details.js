d6963db9493dfb7d7756ac5b3f84eac1d5c943db
Steven <steven.hill1@gmail.com>
2019-08-15 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5894,
-	"created_at": "Wed Jul 31 01:10:15 +0000 2019",
+	"tweets": 5895,
+	"created_at": "Thu Aug 15 01:10:12 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_08.js
@@ -0,0 +1,128 @@
+Grailbird.data.tweets_2019_08 = 
+[
+	{
+		"created_at": "Wed Aug 14 13:42:24 +0000 2019",
+		"id": 1161634176388522000,
+		"id_str": "1161634176388521990",
+		"full_text": "Want to come and work in my team in @ResEngland? I am recruiting into 2 roles: Associate Director of Research Environment (https://t.co/RAquRPFJ70) and Head of Research Assessment Policy (https://t.co/KFQLpEZveX) Closing date: 1 Sep",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						36,
+						47
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/RAquRPFJ70",
+					"expanded_url": "http://www.topcareer.jobs/Vacancy/irc250617_9814.aspx",
+					"display_url": "topcareer.jobs/Vacancy/irc250…",
+					"indices": [
+						123,
+						146
+					]
+				},
+				{
+					"url": "https://t.co/KFQLpEZveX",
+					"expanded_url": "http://www.topcareer.jobs/Vacancy/irc250618_9817.aspx",
+					"display_url": "topcareer.jobs/Vacancy/irc250…",
+					"indices": [
+						188,
+						211
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4623,
+			"friends_count": 976,
+			"listed_count": 180,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 72,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5821,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Want to come and work in my team in @ResEngland? I am recruiting into 2 roles: Associate Director of Research Environment (https://t.co/RAquRPFJ70) and Head of Research Assessment Policy (https://t.co/KFQLpEZveX) Closing date: 1 Sep"
+	}
+]
