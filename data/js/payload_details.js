9a4e1cc7903108f4cf034c39e6520ee016c6131e
Steven <steven.hill1@gmail.com>
2019-08-18 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5896,
-	"created_at": "Sat Aug 17 01:10:12 +0000 2019",
+	"tweets": 5897,
+	"created_at": "Sun Aug 18 01:10:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 17 13:07:12 +0000 2019",
+		"id": 1162703469385572400,
+		"id_str": "1162712483234082816",
+		"full_text": "In @UCL careers framework it’s states “All research outputs are available through Open Access wherever possible.”\n\nhttps://t.co/ijgIB9Sl6R\n\n@ucylpay https://t.co/j4dBgNxfHW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ucl",
+					"name": "UCL",
+					"id": 322601789,
+					"id_str": "322601789",
+					"indices": [
+						3,
+						7
+					]
+				},
+				{
+					"screen_name": "ucylpay",
+					"name": "Paul Ayris",
+					"id": 47042682,
+					"id_str": "47042682",
+					"indices": [
+						140,
+						148
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ijgIB9Sl6R",
+					"expanded_url": "https://www.ucl.ac.uk/human-resources/sites/human-resources/files/ucl-130418.pdf",
+					"display_url": "ucl.ac.uk/human-resource…",
+					"indices": [
+						115,
+						138
+					]
+				},
+				{
+					"url": "https://t.co/j4dBgNxfHW",
+					"expanded_url": "https://twitter.com/stevenhill/status/1162666629098090496",
+					"display_url": "twitter.com/stevenhill/sta…",
+					"indices": [
+						149,
+						172
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1478149944,
+			"id_str": "1478149944",
+			"name": "David Price",
+			"screen_name": "DavidPriceUCL",
+			"location": "UCL",
+			"description": "Professor of Mineral Physics and Vice-Provost (Research), UCL. Views all my own!",
+			"url": "https://t.co/Y5G1vBiAKs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Y5G1vBiAKs",
+							"expanded_url": "https://www.ucl.ac.uk/research/about/professor-david-price-ucl-vice-provost-research",
+							"display_url": "ucl.ac.uk/research/about…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3513,
+			"friends_count": 610,
+			"listed_count": 212,
+			"created_at": "Sun Jun 02 20:44:43 +0000 2013",
+			"favourites_count": 16244,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61117,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DDE6EB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/716696047272259589/9GtOBhz3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/716696047272259589/9GtOBhz3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1478149944/1372516227",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1162666629098090500,
+		"quoted_status_id_str": "1162666629098090496",
+		"quoted_status_permalink": {
+			"url": "https://t.co/j4dBgNxfHW",
+			"expanded": "https://twitter.com/stevenhill/status/1162666629098090496",
+			"display": "twitter.com/stevenhill/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Aug 17 10:05:00 +0000 2019",
+			"id": 1162666629098090500,
+			"id_str": "1162666629098090496",
+			"full_text": "from yesterday: Open access and academic progression https://t.co/1dFkDtiMF0",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				76
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/1dFkDtiMF0",
+						"expanded_url": "https://buff.ly/2OXENtn",
+						"display_url": "buff.ly/2OXENtn",
+						"indices": [
+							53,
+							76
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 7731752,
+				"id_str": "7731752",
+				"name": "Steven Hill",
+				"screen_name": "stevenhill",
+				"location": "Wiltshire, UK",
+				"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+				"url": "https://t.co/pEfaOcJfMm",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/pEfaOcJfMm",
+								"expanded_url": "http://stevenhill.org.uk/",
+								"display_url": "stevenhill.org.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4630,
+				"friends_count": 976,
+				"listed_count": 180,
+				"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+				"favourites_count": 73,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5821,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+				"profile_link_color": "0000FF",
+				"profile_sidebar_border_color": "87BC44",
+				"profile_sidebar_fill_color": "E0FF92",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In @UCL careers framework it’s states “All research outputs are available through Open Access wherever possible.”\n\nhttps://t.co/ijgIB9Sl6R\n\n@ucylpay https://t.co/j4dBgNxfHW"
+	},
+	{
 		"created_at": "Fri Aug 16 11:14:50 +0000 2019",
 		"id": 1162321814942560300,
 		"id_str": "1162321814942560256",
