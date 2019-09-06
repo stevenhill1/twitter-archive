54c2848d787b3d66210d3fca746d90d799706387
Steven <steven.hill1@gmail.com>
2019-09-06 02:10:13 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5912,
-	"created_at": "Thu Sep 05 01:10:12 +0000 2019",
+	"tweets": 5914,
+	"created_at": "Fri Sep 06 01:10:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,507 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Thu Sep 05 20:59:47 +0000 2019",
+		"id": 1169716782678126600,
+		"id_str": "1169716782678126592",
+		"full_text": "Really pleased to see this launched, and to have played a small part in moving this important agenda forwards . Fantastic work from my colleague @louiseaevans2 https://t.co/e7dZlPkh6D",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "louiseaevans2",
+					"name": "Louise Evans",
+					"id": 3306837785,
+					"id_str": "3306837785",
+					"indices": [
+						145,
+						159
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/e7dZlPkh6D",
+					"expanded_url": "https://twitter.com/UKRI_News/status/1169579703923073024",
+					"display_url": "twitter.com/UKRI_News/stat…",
+					"indices": [
+						160,
+						183
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4645,
+			"friends_count": 974,
+			"listed_count": 181,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 73,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5842,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1169579703923073000,
+		"quoted_status_id_str": "1169579703923073024",
+		"quoted_status_permalink": {
+			"url": "https://t.co/e7dZlPkh6D",
+			"expanded": "https://twitter.com/UKRI_News/status/1169579703923073024",
+			"display": "twitter.com/UKRI_News/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Sep 05 11:55:05 +0000 2019",
+			"id": 1169579703923073000,
+			"id_str": "1169579703923073024",
+			"full_text": "UKRI is launching a new £500,000 funding opportunity for research organisations to pilot place-based public engagement partnerships and activities focussed on under-served communities.\nhttps://t.co/mJIMpYUqlf https://t.co/h9kFQaw4Et",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				208
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/mJIMpYUqlf",
+						"expanded_url": "https://www.ukri.org/news/enhancing-place-based-partnerships-in-public-engagement/?previewID=94E71890-04E1-42E4-A4720A5A2A4E286C",
+						"display_url": "ukri.org/news/enhancing…",
+						"indices": [
+							185,
+							208
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1169579696297795600,
+						"id_str": "1169579696297795584",
+						"indices": [
+							209,
+							232
+						],
+						"media_url": "http://pbs.twimg.com/media/EDsuiX5W4AAb_GJ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EDsuiX5W4AAb_GJ.jpg",
+						"url": "https://t.co/h9kFQaw4Et",
+						"display_url": "pic.twitter.com/h9kFQaw4Et",
+						"expanded_url": "https://twitter.com/UKRI_News/status/1169579703923073024/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 349,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 970,
+								"h": 498,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 970,
+								"h": 498,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1169579696297795600,
+						"id_str": "1169579696297795584",
+						"indices": [
+							209,
+							232
+						],
+						"media_url": "http://pbs.twimg.com/media/EDsuiX5W4AAb_GJ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EDsuiX5W4AAb_GJ.jpg",
+						"url": "https://t.co/h9kFQaw4Et",
+						"display_url": "pic.twitter.com/h9kFQaw4Et",
+						"expanded_url": "https://twitter.com/UKRI_News/status/1169579703923073024/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 349,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 970,
+								"h": 498,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 970,
+								"h": 498,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 732141019114930200,
+				"id_str": "732141019114930176",
+				"name": "UKRI",
+				"screen_name": "UKRI_News",
+				"location": "United Kingdom",
+				"description": "With a £7bn budget, UKRI brings together the seven Research Councils, Innovate UK and Research England to support world-leading research and innovation.",
+				"url": "https://t.co/gIusFh73sl",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/gIusFh73sl",
+								"expanded_url": "http://www.ukri.org",
+								"display_url": "ukri.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 27608,
+				"friends_count": 403,
+				"listed_count": 303,
+				"created_at": "Mon May 16 09:29:56 +0000 2016",
+				"favourites_count": 1359,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 2831,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1061931362947682304/K4q5GOXe_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1061931362947682304/K4q5GOXe_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/732141019114930176/1562228444",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 61,
+			"favorite_count": 60,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Really pleased to see this launched, and to have played a small part in moving this important agenda forwards . Fantastic work from my colleague @louiseaevans2 https://t.co/e7dZlPkh6D"
+	},
+	{
+		"created_at": "Thu Sep 05 20:41:27 +0000 2019",
+		"id": 1169637223433670700,
+		"id_str": "1169712165156208640",
+		"full_text": "New blog from @stevenhill now live:\n\"Research integrity is central to maintaining trust in the research system for researchers, funders and the public alike\"\nhttps://t.co/rjB4YtpPzX https://t.co/i6JKimqmug",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stevenhill",
+					"name": "Steven Hill",
+					"id": 7731752,
+					"id_str": "7731752",
+					"indices": [
+						14,
+						25
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/rjB4YtpPzX",
+					"expanded_url": "https://re.ukri.org/blog/examining-the-effect-of-incentives-in-the-research-system-on-research-integrity/",
+					"display_url": "re.ukri.org/blog/examining…",
+					"indices": [
+						158,
+						181
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1169637204076970000,
+					"id_str": "1169637204076969985",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/EDti1xEWwAEk-Op.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDti1xEWwAEk-Op.jpg",
+					"url": "https://t.co/i6JKimqmug",
+					"display_url": "pic.twitter.com/i6JKimqmug",
+					"expanded_url": "https://twitter.com/ResEngland/status/1169637223433670657/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 685,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 388,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1169,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1169637204076970000,
+					"id_str": "1169637204076969985",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/EDti1xEWwAEk-Op.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDti1xEWwAEk-Op.jpg",
+					"url": "https://t.co/i6JKimqmug",
+					"display_url": "pic.twitter.com/i6JKimqmug",
+					"expanded_url": "https://twitter.com/ResEngland/status/1169637223433670657/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 685,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 388,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1169,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 823853384138637300,
+			"id_str": "823853384138637313",
+			"name": "Research England",
+			"screen_name": "ResEngland",
+			"location": "Bristol",
+			"description": "We create and sustain the conditions for a healthy, dynamic research and knowledge exchange system in England's universities. Part of @UKRI_News",
+			"url": "https://t.co/BI90gIR8Ar",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BI90gIR8Ar",
+							"expanded_url": "https://re.ukri.org/",
+							"display_url": "re.ukri.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7751,
+			"friends_count": 359,
+			"listed_count": 87,
+			"created_at": "Tue Jan 24 11:22:08 +0000 2017",
+			"favourites_count": 1240,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1126,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1006515717888512000/KnXg55bl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1006515717888512000/KnXg55bl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/823853384138637313/1553858043",
+			"profile_link_color": "ACBD37",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New blog from @stevenhill now live:\n\"Research integrity is central to maintaining trust in the research system for researchers, funders and the public alike\"\nhttps://t.co/rjB4YtpPzX https://t.co/i6JKimqmug"
+	},
+	{
 		"created_at": "Wed Sep 04 10:58:08 +0000 2019",
 		"id": 1169202983189569500,
 		"id_str": "1169202983189569537",
