0dcd70c72acc0aaf42f40b7ee2e7f81e819cc5b5
Steven <steven.hill1@gmail.com>
2019-10-05 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5924,
-	"created_at": "Fri Oct 04 01:10:13 +0000 2019",
+	"tweets": 5925,
+	"created_at": "Sat Oct 05 01:10:13 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Fri Oct 04 17:20:33 +0000 2019",
+		"id": 1180060376727261200,
+		"id_str": "1180170855390822406",
+		"full_text": "Thinking about the future of research assessment:  https://t.co/8BgFpIOnmD\n\nNew blog by @stevenhill on the day that we published a report carried out by @RANDEurope on developments in the research landscape and their potential impact on research assessment in the next 5-10 years. https://t.co/CTuRzQrtxX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stevenhill",
+					"name": "Steven Hill",
+					"id": 7731752,
+					"id_str": "7731752",
+					"indices": [
+						88,
+						99
+					]
+				},
+				{
+					"screen_name": "RANDEurope",
+					"name": "RAND Europe",
+					"id": 22954137,
+					"id_str": "22954137",
+					"indices": [
+						153,
+						164
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8BgFpIOnmD",
+					"expanded_url": "https://bit.ly/2AFZTlZ",
+					"display_url": "bit.ly/2AFZTlZ",
+					"indices": [
+						51,
+						74
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1180060367881457700,
+					"id_str": "1180060367881457665",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/EGBqpwEXUAE5w26.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGBqpwEXUAE5w26.jpg",
+					"url": "https://t.co/CTuRzQrtxX",
+					"display_url": "pic.twitter.com/CTuRzQrtxX",
+					"expanded_url": "https://twitter.com/ResEngland/status/1180060376727261184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1180060367881457700,
+					"id_str": "1180060367881457665",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/EGBqpwEXUAE5w26.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGBqpwEXUAE5w26.jpg",
+					"url": "https://t.co/CTuRzQrtxX",
+					"display_url": "pic.twitter.com/CTuRzQrtxX",
+					"expanded_url": "https://twitter.com/ResEngland/status/1180060376727261184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 823853384138637300,
+			"id_str": "823853384138637313",
+			"name": "Research England",
+			"screen_name": "ResEngland",
+			"location": "Bristol",
+			"description": "We create and sustain the conditions for a healthy, dynamic research and knowledge exchange system in England's universities. Part of @UKRI_News",
+			"url": "https://t.co/BI90gIR8Ar",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BI90gIR8Ar",
+							"expanded_url": "https://re.ukri.org/",
+							"display_url": "re.ukri.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7994,
+			"friends_count": 359,
+			"listed_count": 90,
+			"created_at": "Tue Jan 24 11:22:08 +0000 2017",
+			"favourites_count": 1277,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1150,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1006515717888512000/KnXg55bl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1006515717888512000/KnXg55bl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/823853384138637313/1553858043",
+			"profile_link_color": "ACBD37",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thinking about the future of research assessment:  https://t.co/8BgFpIOnmD\n\nNew blog by @stevenhill on the day that we published a report carried out by @RANDEurope on developments in the research landscape and their potential impact on research assessment in the next 5-10 years. https://t.co/CTuRzQrtxX"
+	},
+	{
 		"created_at": "Thu Oct 03 12:57:25 +0000 2019",
 		"id": 1179742249590104000,
 		"id_str": "1179742249590104065",
