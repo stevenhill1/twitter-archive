1bb31e4ce51f2fa21af97761817c149cbf43b5de
Steven <steven.hill1@gmail.com>
2019-11-01 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5929,
-	"created_at": "Tue Oct 22 01:10:12 +0000 2019",
+	"tweets": 5930,
+	"created_at": "Fri Nov 01 01:10:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,281 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Thu Oct 31 16:21:37 +0000 2019",
+		"id": 1189929836153884700,
+		"id_str": "1189940497885859840",
+		"full_text": "A great new blog from @stevenhill from @ResEngland and @NicTurner2013 from @officestudents, discussing access and success for black, Asian and minority ethnicity groups in postgraduate research study.\n\nRead: https://t.co/OwtORcAfBM https://t.co/tGK72HiS3A",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			231
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stevenhill",
+					"name": "Steven Hill",
+					"id": 7731752,
+					"id_str": "7731752",
+					"indices": [
+						22,
+						33
+					]
+				},
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						39,
+						50
+					]
+				},
+				{
+					"screen_name": "NicTurner2013",
+					"name": "nicola turner",
+					"id": 1267835101,
+					"id_str": "1267835101",
+					"indices": [
+						55,
+						69
+					]
+				},
+				{
+					"screen_name": "officestudents",
+					"name": "The Office for Students (OfS)",
+					"id": 801389270485307400,
+					"id_str": "801389270485307392",
+					"indices": [
+						75,
+						90
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OwtORcAfBM",
+					"expanded_url": "http://bit.ly/2WEH7G1",
+					"display_url": "bit.ly/2WEH7G1",
+					"indices": [
+						208,
+						231
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1189929823688446000,
+					"id_str": "1189929823688445952",
+					"indices": [
+						232,
+						255
+					],
+					"media_url": "http://pbs.twimg.com/media/EIN63sCX0AAUd2S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIN63sCX0AAUd2S.jpg",
+					"url": "https://t.co/tGK72HiS3A",
+					"display_url": "pic.twitter.com/tGK72HiS3A",
+					"expanded_url": "https://twitter.com/ResEngland/status/1189929836153884672/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"all": {
+							"tags": [
+								{
+									"type": "user",
+									"user_id": "732141019114930176",
+									"screen_name": "UKRI_News",
+									"name": "UK Research and Innovation"
+								}
+							]
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1189929823688446000,
+					"id_str": "1189929823688445952",
+					"indices": [
+						232,
+						255
+					],
+					"media_url": "http://pbs.twimg.com/media/EIN63sCX0AAUd2S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIN63sCX0AAUd2S.jpg",
+					"url": "https://t.co/tGK72HiS3A",
+					"display_url": "pic.twitter.com/tGK72HiS3A",
+					"expanded_url": "https://twitter.com/ResEngland/status/1189929836153884672/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"all": {
+							"tags": [
+								{
+									"type": "user",
+									"user_id": "732141019114930176",
+									"screen_name": "UKRI_News",
+									"name": "UK Research and Innovation"
+								}
+							]
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 823853384138637300,
+			"id_str": "823853384138637313",
+			"name": "Research England",
+			"screen_name": "ResEngland",
+			"location": "Bristol",
+			"description": "We create and sustain the conditions for a healthy, dynamic research and knowledge exchange system in England's universities. We are part of @UKRI_News.",
+			"url": "https://t.co/BI90gIR8Ar",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BI90gIR8Ar",
+							"expanded_url": "https://re.ukri.org/",
+							"display_url": "re.ukri.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8212,
+			"friends_count": 361,
+			"listed_count": 91,
+			"created_at": "Tue Jan 24 11:22:08 +0000 2017",
+			"favourites_count": 1283,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1164,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1182198326814560256/-DfFd5ES_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1182198326814560256/-DfFd5ES_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/823853384138637313/1570693021",
+			"profile_link_color": "ACBD37",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A great new blog from @stevenhill from @ResEngland and @NicTurner2013 from @officestudents, discussing access and success for black, Asian and minority ethnicity groups in postgraduate research study.\n\nRead: https://t.co/OwtORcAfBM https://t.co/tGK72HiS3A"
+	},
+	{
 		"created_at": "Mon Oct 21 19:22:24 +0000 2019",
 		"id": 1186274796105019400,
 		"id_str": "1186362114056867842",
