c63afd261d1303dcc9a5c6f1037fe0f284e29db9
Steven <steven.hill1@gmail.com>
2020-01-05 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5931,
-	"created_at": "Wed Nov 06 01:10:12 +0000 2019",
+	"tweets": 5932,
+	"created_at": "Sun Jan 05 01:10:12 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_01.js
@@ -0,0 +1,340 @@
+Grailbird.data.tweets_2020_01 = 
+[
+	{
+		"created_at": "Sat Jan 04 15:23:44 +0000 2020",
+		"id": 1213481140876656600,
+		"id_str": "1213481140876656640",
+		"full_text": "Important findings https://t.co/Xpvr5pVzWU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			18
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Xpvr5pVzWU",
+					"expanded_url": "https://twitter.com/vtraag/status/1207579851168845824",
+					"display_url": "twitter.com/vtraag/status/…",
+					"indices": [
+						19,
+						42
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4757,
+			"friends_count": 723,
+			"listed_count": 183,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 73,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5789,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1207579851168845800,
+		"quoted_status_id_str": "1207579851168845824",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Xpvr5pVzWU",
+			"expanded": "https://twitter.com/vtraag/status/1207579851168845824",
+			"display": "twitter.com/vtraag/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Dec 19 08:34:06 +0000 2019",
+			"id": 1207579851168845800,
+			"id_str": "1207579851168845824",
+			"full_text": "Articles in high-impact journals are highly cited. But why? Are they somehow \"better\"? Or is it just because they are published in a high-impact journal? The causality is difficult to establish, but in my latest preprint I find: it's both.\nhttps://t.co/9T1Rax4pAN (1/8) https://t.co/pYV2KNN1FV",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				269
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/9T1Rax4pAN",
+						"expanded_url": "https://arxiv.org/abs/1912.08648",
+						"display_url": "arxiv.org/abs/1912.08648",
+						"indices": [
+							240,
+							263
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1207579845431111700,
+						"id_str": "1207579845431111680",
+						"indices": [
+							270,
+							293
+						],
+						"media_url": "http://pbs.twimg.com/media/EMIveKzXsAA7Y2e.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EMIveKzXsAA7Y2e.jpg",
+						"url": "https://t.co/pYV2KNN1FV",
+						"display_url": "pic.twitter.com/pYV2KNN1FV",
+						"expanded_url": "https://twitter.com/vtraag/status/1207579851168845824/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 245,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 738,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 433,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							},
+							"large": {
+								"faces": []
+							},
+							"medium": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1207579845431111700,
+						"id_str": "1207579845431111680",
+						"indices": [
+							270,
+							293
+						],
+						"media_url": "http://pbs.twimg.com/media/EMIveKzXsAA7Y2e.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EMIveKzXsAA7Y2e.jpg",
+						"url": "https://t.co/pYV2KNN1FV",
+						"display_url": "pic.twitter.com/pYV2KNN1FV",
+						"expanded_url": "https://twitter.com/vtraag/status/1207579851168845824/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 245,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 738,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 433,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							},
+							"large": {
+								"faces": []
+							},
+							"medium": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 293983295,
+				"id_str": "293983295",
+				"name": "Vincent Traag",
+				"screen_name": "vtraag",
+				"location": "Netherlands",
+				"description": "Researcher @CWTSLeiden. Computational social sciences, networks and modelling. Background in math & sociology. https://t.co/phx9slUJTf",
+				"url": "https://t.co/Kj7aXuIleP",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Kj7aXuIleP",
+								"expanded_url": "http://www.traag.net",
+								"display_url": "traag.net",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/phx9slUJTf",
+								"expanded_url": "http://keybase.io/vtraag",
+								"display_url": "keybase.io/vtraag",
+								"indices": [
+									111,
+									134
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1595,
+				"friends_count": 851,
+				"listed_count": 91,
+				"created_at": "Fri May 06 09:58:06 +0000 2011",
+				"favourites_count": 2521,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4526,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/573868733954854913/fcHsu5hb_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/573868733954854913/fcHsu5hb_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/293983295/1399879231",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 158,
+			"favorite_count": 319,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 8,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Important findings https://t.co/Xpvr5pVzWU"
+	}
+]
