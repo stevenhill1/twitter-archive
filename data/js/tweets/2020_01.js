d6472950217c9f0500272f6b43be0c65dde44df3
Steven <steven.hill1@gmail.com>
2020-01-19 01:10:13 +0000

    Added 1 tweets

--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Sat Jan 18 18:50:58 +0000 2020",
+		"id": 1218606724950712300,
+		"id_str": "1218606724950712325",
+		"full_text": "This new collection looks really interesting. So much to read, so little time… https://t.co/Cma3vQ0f7I",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			78
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Cma3vQ0f7I",
+					"expanded_url": "https://twitter.com/JoannaChataway/status/1218551831191785472",
+					"display_url": "twitter.com/JoannaChataway…",
+					"indices": [
+						79,
+						102
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4766,
+			"friends_count": 725,
+			"listed_count": 183,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5802,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1218551831191785500,
+		"quoted_status_id_str": "1218551831191785472",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Cma3vQ0f7I",
+			"expanded": "https://twitter.com/JoannaChataway/status/1218551831191785472",
+			"display": "twitter.com/JoannaChataway…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Jan 18 15:12:50 +0000 2020",
+			"id": 1218551831191785500,
+			"id_str": "1218551831191785472",
+			"full_text": "Great to have a chapter, written with @chuxdaniels, in this new OA book, 'Transforming Research Excellence: New ideas from the Global South'.  Some wonderful academics and research policy/funder colleagues share insights and ideas about ways forward.  https://t.co/ZSdZG9J3PI",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				275
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "chuxdaniels",
+						"name": "Chux Daniels",
+						"id": 32697764,
+						"id_str": "32697764",
+						"indices": [
+							38,
+							50
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/ZSdZG9J3PI",
+						"expanded_url": "http://www.africanminds.co.za/dd-product/transforming-research-excellence-new-ideas-from-the-global-south/",
+						"display_url": "africanminds.co.za/dd-product/tra…",
+						"indices": [
+							252,
+							275
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1245656534,
+				"id_str": "1245656534",
+				"name": "Joanna Chataway",
+				"screen_name": "JoannaChataway",
+				"location": "London, England",
+				"description": "Head of Department, Science, Technology, Engineering and Public Policy (STEaPP), UCL. Views my own.",
+				"url": "https://t.co/Qw7zuWBbwn",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Qw7zuWBbwn",
+								"expanded_url": "https://www.ucl.ac.uk/steapp/",
+								"display_url": "ucl.ac.uk/steapp/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1275,
+				"friends_count": 903,
+				"listed_count": 41,
+				"created_at": "Wed Mar 06 09:11:38 +0000 2013",
+				"favourites_count": 6511,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4744,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/551680506090516480/VHkvIysT_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/551680506090516480/VHkvIysT_normal.jpeg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This new collection looks really interesting. So much to read, so little time… https://t.co/Cma3vQ0f7I"
+	},
+	{
 		"created_at": "Fri Jan 17 20:28:10 +0000 2020",
 		"id": 1218201338762473500,
 		"id_str": "1218268800317034496",
