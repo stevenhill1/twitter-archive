c26d5c9a270511c7822af737ffaeb9ccfacc2bfc
Steven <steven.hill1@gmail.com>
2020-01-24 01:10:14 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5948,
-	"created_at": "Thu Jan 23 01:10:13 +0000 2020",
+	"tweets": 5949,
+	"created_at": "Fri Jan 24 01:10:13 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Thu Jan 23 18:24:58 +0000 2020",
+		"id": 1220256748734382000,
+		"id_str": "1220412120308899841",
+		"full_text": "Substantial increase expected in number of staff submitted in REF 2021.  Read the news item here: https://t.co/4tilOtWDGe @ResEngland @HEFCW @ScotFundCouncil @Economy_NI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						122,
+						133
+					]
+				},
+				{
+					"screen_name": "HEFCW",
+					"name": "HEFCW | CCAUC",
+					"id": 3327141628,
+					"id_str": "3327141628",
+					"indices": [
+						134,
+						140
+					]
+				},
+				{
+					"screen_name": "ScotFundCouncil",
+					"name": "Scottish Funding Council",
+					"id": 325622395,
+					"id_str": "325622395",
+					"indices": [
+						141,
+						157
+					]
+				},
+				{
+					"screen_name": "Economy_NI",
+					"name": "Economy NI",
+					"id": 4871932901,
+					"id_str": "4871932901",
+					"indices": [
+						158,
+						169
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/4tilOtWDGe",
+					"expanded_url": "http://bit.ly/30MAIKJ",
+					"display_url": "bit.ly/30MAIKJ",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 822173125639110700,
+			"id_str": "822173125639110657",
+			"name": "REF2021",
+			"screen_name": "REF_2021",
+			"location": "United Kingdom",
+			"description": "Official account of the Research Excellence Framework 2021, undertaken by the four UK HE funding bodies. Twitter page managed by the REF team at @ResEngland.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7121,
+			"friends_count": 1,
+			"listed_count": 57,
+			"created_at": "Thu Jan 19 20:05:23 +0000 2017",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 69,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/903598145699020802/ad8FE4_d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/903598145699020802/ad8FE4_d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/822173125639110657/1504267792",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 36,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Substantial increase expected in number of staff submitted in REF 2021.  Read the news item here: https://t.co/4tilOtWDGe @ResEngland @HEFCW @ScotFundCouncil @Economy_NI"
+	},
+	{
 		"created_at": "Wed Jan 22 21:50:39 +0000 2020",
 		"id": 1220033646138138600,
 		"id_str": "1220101496907489280",
