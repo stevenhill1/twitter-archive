ec7eaf1b6b5a4bf9939e90b1058dbe2a468a4bc4
Steven <steven.hill1@gmail.com>
2020-02-21 01:10:14 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5960,
-	"created_at": "Thu Feb 20 01:10:12 +0000 2020",
+	"tweets": 5961,
+	"created_at": "Fri Feb 21 01:10:12 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Thu Feb 20 20:42:03 +0000 2020",
+		"id": 1230547369449508900,
+		"id_str": "1230593477475524619",
+		"full_text": "Enjoyed my first meeting today as a Council member for @ResEngland. Big thanks to @dsweeneyUK, RE team and fellow council members for being so kind and welcoming - and patient!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			176
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						55,
+						66
+					]
+				},
+				{
+					"screen_name": "dsweeneyUK",
+					"name": "David Sweeney",
+					"id": 4162904121,
+					"id_str": "4162904121",
+					"indices": [
+						82,
+						93
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 234912121,
+			"id_str": "234912121",
+			"name": "Kellie Beirne",
+			"screen_name": "kelliebeirne",
+			"location": "",
+			"description": "Mum. Director @CCRCityDeal. Chair of Innovation Advisory Council Wales and Member of Science Advisory Council Wales. Verbs more important than nouns.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3924,
+			"friends_count": 3866,
+			"listed_count": 100,
+			"created_at": "Thu Jan 06 21:55:46 +0000 2011",
+			"favourites_count": 11324,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9934,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1096850403826782216/lmytvTnC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1096850403826782216/lmytvTnC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/234912121/1506675376",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "381b5744987ae0bd",
+			"url": "https://api.twitter.com/1.1/geo/id/381b5744987ae0bd.json",
+			"place_type": "city",
+			"name": "Swindon",
+			"full_name": "Swindon, England",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-1.8519024,
+							51.535198
+						],
+						[
+							-1.719477,
+							51.535198
+						],
+						[
+							-1.719477,
+							51.6108189
+						],
+						[
+							-1.8519024,
+							51.6108189
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Enjoyed my first meeting today as a Council member for @ResEngland. Big thanks to @dsweeneyUK, RE team and fellow council members for being so kind and welcoming - and patient!"
+	},
+	{
 		"created_at": "Wed Feb 19 13:03:13 +0000 2020",
 		"id": 1230074585653141500,
 		"id_str": "1230115623730110466",
