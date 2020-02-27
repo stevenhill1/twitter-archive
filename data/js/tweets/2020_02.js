45cff1a7ad19aedf81dc1e10a269b816ff9e2094
Steven <steven.hill1@gmail.com>
2020-02-27 01:10:14 +0000

    Added 1 tweets

--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,174 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Wed Feb 26 15:00:40 +0000 2020",
+		"id": 1232681895063904300,
+		"id_str": "1232681895063904259",
+		"full_text": "@LeonievanDrooge @RathenauNL @WoutScholten @Paul_Diederen Excellence as a threshold in the case of E3 - credibility of expansion plans, future sustainability and the uniqueness of the research area were important criteria",
+		"truncated": false,
+		"display_text_range": [
+			58,
+			221
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LeonievanDrooge",
+					"name": "Leonie van Drooge",
+					"id": 324897261,
+					"id_str": "324897261",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "RathenauNL",
+					"name": "Rathenau Instituut",
+					"id": 28317115,
+					"id_str": "28317115",
+					"indices": [
+						17,
+						28
+					]
+				},
+				{
+					"screen_name": "WoutScholten",
+					"name": "Wout Scholten",
+					"id": 1917131712,
+					"id_str": "1917131712",
+					"indices": [
+						29,
+						42
+					]
+				},
+				{
+					"screen_name": "Paul_Diederen",
+					"name": "Paul Diederen",
+					"id": 33167678,
+					"id_str": "33167678",
+					"indices": [
+						43,
+						57
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1232662741971853300,
+		"in_reply_to_status_id_str": "1232662741971853317",
+		"in_reply_to_user_id": 324897261,
+		"in_reply_to_user_id_str": "324897261",
+		"in_reply_to_screen_name": "LeonievanDrooge",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4812,
+			"friends_count": 730,
+			"listed_count": 182,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 82,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5833,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "53b67b1d1cc81a51",
+			"url": "https://api.twitter.com/1.1/geo/id/53b67b1d1cc81a51.json",
+			"place_type": "city",
+			"name": "Birmingham",
+			"full_name": "Birmingham, England",
+			"country_code": "GB",
+			"country": "United Kingdom",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-2.033651,
+							52.381063
+						],
+						[
+							-1.74763,
+							52.381063
+						],
+						[
+							-1.74763,
+							52.60687
+						],
+						[
+							-2.033651,
+							52.60687
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@LeonievanDrooge @RathenauNL @WoutScholten @Paul_Diederen Excellence as a threshold in the case of E3 - credibility of expansion plans, future sustainability and the uniqueness of the research area were important criteria"
+	},
+	{
 		"created_at": "Tue Feb 25 16:33:57 +0000 2020",
 		"id": 1232342982734512000,
 		"id_str": "1232342982734512128",
@@ -248,7 +416,7 @@ Grailbird.data.tweets_2020_02 =
 				"name": "Geoff Hill",
 				"screen_name": "geowonks",
 				"location": "Bristol, England",
-				"description": "Senior Policy Advisor @ResEngland @UKRI_News - lapsed palaeo academic. 🏳️‍🌈",
+				"description": "Senior Policy Advisor @ResEngland @UKRI_News - lapsed palaeo academic. 🏳️🌈",
 				"url": null,
 				"entities": {
 					"description": {
