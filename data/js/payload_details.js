7f52ddc0d5edfdddbb7845702c39402bf9979d0e
Steven <steven.hill1@gmail.com>
2020-04-05 02:10:14 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5992,
-	"created_at": "Mon Mar 30 01:10:13 +0000 2020",
+	"tweets": 5993,
+	"created_at": "Sun Apr 05 01:10:13 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_04.js
@@ -0,0 +1,277 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Sat Apr 04 12:37:51 +0000 2020",
+		"id": 1246416694186311700,
+		"id_str": "1246416694186311682",
+		"full_text": "HESA have sufficiently granular data to answer (staff record includes Standard Occupational Classification), but on a quick look doesn’t seem to be publicly available. Might be on HEIDI if you have access. https://t.co/IdOF73U0xk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			205
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IdOF73U0xk",
+					"expanded_url": "https://twitter.com/jameswilsdon/status/1246408524097302528",
+					"display_url": "twitter.com/jameswilsdon/s…",
+					"indices": [
+						206,
+						229
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4873,
+			"friends_count": 735,
+			"listed_count": 184,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 83,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5863,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1246408524097302500,
+		"quoted_status_id_str": "1246408524097302528",
+		"quoted_status_permalink": {
+			"url": "https://t.co/IdOF73U0xk",
+			"expanded": "https://twitter.com/jameswilsdon/status/1246408524097302528",
+			"display": "twitter.com/jameswilsdon/s…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Apr 04 12:05:23 +0000 2020",
+			"id": 1246408524097302500,
+			"id_str": "1246408524097302528",
+			"full_text": "Can anyone point me to a source for this stat on HE marketing staff growing 9x faster than academic in Camilla Cavendish’s piece in FT? Alison Wolf uses it too in Prospect but neither links to original data: https://t.co/FlKYJcw0WO @markmleach @dkernohan @natalieday1 @nickhillman https://t.co/gbEakKqAEJ",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "markmleach",
+						"name": "Mark Leach",
+						"id": 21110235,
+						"id_str": "21110235",
+						"indices": [
+							232,
+							243
+						]
+					},
+					{
+						"screen_name": "dkernohan",
+						"name": "David Kernohan",
+						"id": 12219232,
+						"id_str": "12219232",
+						"indices": [
+							244,
+							254
+						]
+					},
+					{
+						"screen_name": "natalieday1",
+						"name": "Natalie Day",
+						"id": 294771724,
+						"id_str": "294771724",
+						"indices": [
+							255,
+							267
+						]
+					},
+					{
+						"screen_name": "nickhillman",
+						"name": "Nick Hillman",
+						"id": 80848486,
+						"id_str": "80848486",
+						"indices": [
+							268,
+							280
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/FlKYJcw0WO",
+						"expanded_url": "https://www.prospectmagazine.co.uk/magazine/the-uk-university-admissions-system-doesnt-make-the-grade-higher-education-augar-review",
+						"display_url": "prospectmagazine.co.uk/magazine/the-u…",
+						"indices": [
+							208,
+							231
+						]
+					},
+					{
+						"url": "https://t.co/gbEakKqAEJ",
+						"expanded_url": "https://twitter.com/davidbarrie/status/1246345517107499008",
+						"display_url": "twitter.com/davidbarrie/st…",
+						"indices": [
+							281,
+							304
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 276224538,
+				"id_str": "276224538",
+				"name": "James Wilsdon",
+				"screen_name": "jameswilsdon",
+				"location": "Sheffield & Wellcome Trust",
+				"description": "director @RoRInstitute | @digitalsci professor of research policy @sheffielduni | vice-chair @INGSciAdvice",
+				"url": "https://t.co/jxRP8wB5Xo",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/jxRP8wB5Xo",
+								"expanded_url": "https://www.sheffield.ac.uk/politics/people/academic/james-wilsdon",
+								"display_url": "sheffield.ac.uk/politics/peopl…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10039,
+				"friends_count": 1685,
+				"listed_count": 0,
+				"created_at": "Sat Apr 02 22:47:09 +0000 2011",
+				"favourites_count": 5903,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 15212,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "BADFCD",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1151830369987178496/MVGhpTqq_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1151830369987178496/MVGhpTqq_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/276224538/1579965225",
+				"profile_link_color": "FF002B",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFF7CC",
+				"profile_text_color": "0C3E53",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1246345517107499000,
+			"quoted_status_id_str": "1246345517107499008",
+			"quoted_status_permalink": {
+				"url": "https://t.co/gbEakKqAEJ",
+				"expanded": "https://twitter.com/davidbarrie/status/1246345517107499008",
+				"display": "twitter.com/davidbarrie/st…"
+			},
+			"retweet_count": 4,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "HESA have sufficiently granular data to answer (staff record includes Standard Occupational Classification), but on a quick look doesn’t seem to be publicly available. Might be on HEIDI if you have access. https://t.co/IdOF73U0xk"
+	}
+]
