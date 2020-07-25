93420a7062f01707f18f2427c02f8e23c0a4d15d
Steven <steven.hill1@gmail.com>
2020-07-25 02:10:14 +0100

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6013,
-	"created_at": "Fri Jul 24 01:10:12 +0000 2020",
+	"tweets": 6016,
+	"created_at": "Sat Jul 25 01:10:13 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,530 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Fri Jul 24 13:40:22 +0000 2020",
+		"id": 1286657478072180700,
+		"id_str": "1286657478072180736",
+		"full_text": "@wjbfarrell Fair enough, though I think the waste of researcher time is still \npertinent. Can you point me to a source where I can learn more about typesetting and its value?",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			174
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wjbfarrell",
+					"name": "William Farrell",
+					"id": 2821176915,
+					"id_str": "2821176915",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1286654464338931700,
+		"in_reply_to_status_id_str": "1286654464338931713",
+		"in_reply_to_user_id": 2821176915,
+		"in_reply_to_user_id_str": "2821176915",
+		"in_reply_to_screen_name": "wjbfarrell",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4985,
+			"friends_count": 739,
+			"listed_count": 177,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 84,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5884,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@wjbfarrell Fair enough, though I think the waste of researcher time is still \npertinent. Can you point me to a source where I can learn more about typesetting and its value?"
+	},
+	{
+		"created_at": "Fri Jul 24 13:27:37 +0000 2020",
+		"id": 1286638474188198000,
+		"id_str": "1286654270750810116",
+		"full_text": "Six principles for building back better:\n\n- Focus on national wellbeing\n- Reset the relationship between the state and citizens\n- The future can be local\n- Remodel our relationship with work\n- Build financial resilience\n- Technology can be for all https://t.co/c1xhEgb3vY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			247
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/c1xhEgb3vY",
+					"expanded_url": "https://twitter.com/CarnegieUKTrust/status/1286215473432891392",
+					"display_url": "twitter.com/CarnegieUKTrus…",
+					"indices": [
+						248,
+						271
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 69076943,
+			"id_str": "69076943",
+			"name": "Tom Saunders",
+			"screen_name": "Tom_saunders",
+			"location": "London",
+			"description": "Head of Public Engagement at UK Research and Innovation. Trustee at climate charity @_wearepossible. Proud Northerner in London. All views my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3444,
+			"friends_count": 987,
+			"listed_count": 87,
+			"created_at": "Wed Aug 26 19:52:31 +0000 2009",
+			"favourites_count": 3028,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2411,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1213058854663204864/5A3mMIEP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1213058854663204864/5A3mMIEP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/69076943/1586381134",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1286215473432891400,
+		"quoted_status_id_str": "1286215473432891392",
+		"quoted_status_permalink": {
+			"url": "https://t.co/c1xhEgb3vY",
+			"expanded": "https://twitter.com/CarnegieUKTrust/status/1286215473432891392",
+			"display": "twitter.com/CarnegieUKTrus…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Jul 23 08:24:00 +0000 2020",
+			"id": 1286215473432891400,
+			"id_str": "1286215473432891392",
+			"full_text": "NEW REPORT 🥁\n\nToday, we're pleased to publish a report with six propositions to Build Back, for the Better. 👇\n\nhttps://t.co/ONUwE1WO80 https://t.co/xncWxWJC4i",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				134
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ONUwE1WO80",
+						"expanded_url": "http://bit.ly/3hpJKEz",
+						"display_url": "bit.ly/3hpJKEz",
+						"indices": [
+							111,
+							134
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1286211347156541400,
+						"id_str": "1286211347156541440",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1286211347156541440/pu/img/U_ciyE_AIRaJ7OvR.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1286211347156541440/pu/img/U_ciyE_AIRaJ7OvR.jpg",
+						"url": "https://t.co/xncWxWJC4i",
+						"display_url": "pic.twitter.com/xncWxWJC4i",
+						"expanded_url": "https://twitter.com/CarnegieUKTrust/status/1286215473432891392/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1286211347156541400,
+						"id_str": "1286211347156541440",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1286211347156541440/pu/img/U_ciyE_AIRaJ7OvR.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1286211347156541440/pu/img/U_ciyE_AIRaJ7OvR.jpg",
+						"url": "https://t.co/xncWxWJC4i",
+						"display_url": "pic.twitter.com/xncWxWJC4i",
+						"expanded_url": "https://twitter.com/CarnegieUKTrust/status/1286215473432891392/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 60000,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1286211347156541440/pu/vid/640x360/ZkO850P08itf4vVg.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1286211347156541440/pu/pl/UbyFKfWG6qfyumYT.m3u8?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1286211347156541440/pu/vid/1280x720/UTJ5kvOVvk6aCdoR.mp4?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1286211347156541440/pu/vid/480x270/2CoL8jnsc84-JvTL.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 324969914,
+				"id_str": "324969914",
+				"name": "Carnegie UK Trust",
+				"screen_name": "CarnegieUKTrust",
+				"location": "UK",
+				"description": "Tweets from the Carnegie UK Trust. Working to improve the wellbeing of people throughout the UK & Ireland.",
+				"url": "http://t.co/4ism5G8XSu",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/4ism5G8XSu",
+								"expanded_url": "http://www.carnegieuktrust.org.uk",
+								"display_url": "carnegieuktrust.org.uk",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10837,
+				"friends_count": 2617,
+				"listed_count": 264,
+				"created_at": "Mon Jun 27 14:33:47 +0000 2011",
+				"favourites_count": 3232,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 17442,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1133758754242084867/vQT4RwHJ_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1133758754242084867/vQT4RwHJ_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/324969914/1579186357",
+				"profile_link_color": "452266",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 24,
+			"favorite_count": 50,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 4,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Six principles for building back better:\n\n- Focus on national wellbeing\n- Reset the relationship between the state and citizens\n- The future can be local\n- Remodel our relationship with work\n- Build financial resilience\n- Technology can be for all https://t.co/c1xhEgb3vY"
+	},
+	{
+		"created_at": "Fri Jul 24 11:27:07 +0000 2020",
+		"id": 1286623943865049000,
+		"id_str": "1286623943865049088",
+		"full_text": "@samoore_ Recently came across an example where an incorrect reference had been included in the bibliography (same 1st author surname, otherwise totally unrelated)",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			163
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "samoore_",
+					"name": "Samuel Moore",
+					"id": 77294800,
+					"id_str": "77294800",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1286622237529907200,
+		"in_reply_to_status_id_str": "1286622237529907200",
+		"in_reply_to_user_id": 77294800,
+		"in_reply_to_user_id_str": "77294800",
+		"in_reply_to_screen_name": "samoore_",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4985,
+			"friends_count": 739,
+			"listed_count": 177,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 84,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5884,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@samoore_ Recently came across an example where an incorrect reference had been included in the bibliography (same 1st author surname, otherwise totally unrelated)"
+	},
+	{
 		"created_at": "Thu Jul 23 07:09:04 +0000 2020",
 		"id": 1286196615615852500,
 		"id_str": "1286196615615852544",
