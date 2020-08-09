26b57a012844395069cf17800bef37a659c572f8
Steven <steven.hill1@gmail.com>
2020-08-09 02:10:14 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6024,
-	"created_at": "Sat Aug 08 01:10:12 +0000 2020",
+	"tweets": 6025,
+	"created_at": "Sun Aug 09 01:10:12 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 08 10:36:05 +0000 2020",
+		"id": 1291600712665882600,
+		"id_str": "1292046920366592002",
+		"full_text": "In a letter, leading UK scientists have set out seven principles to stamp out bullying and harassment in research, reports @annamckie.\nhttps://t.co/QF8BuCCUxG https://t.co/ZG15Md8lyR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			158
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Annamckie",
+					"name": "annamckie",
+					"id": 39079896,
+					"id_str": "39079896",
+					"indices": [
+						123,
+						133
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QF8BuCCUxG",
+					"expanded_url": "https://bit.ly/3icDoJk",
+					"display_url": "bit.ly/3icDoJk",
+					"indices": [
+						135,
+						158
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1291600710354837500,
+					"id_str": "1291600710354837510",
+					"indices": [
+						159,
+						182
+					],
+					"media_url": "http://pbs.twimg.com/media/EeywALxXoAYOhNL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeywALxXoAYOhNL.jpg",
+					"url": "https://t.co/ZG15Md8lyR",
+					"display_url": "pic.twitter.com/ZG15Md8lyR",
+					"expanded_url": "https://twitter.com/timeshighered/status/1291600712665882624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 620,
+							"h": 413,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 620,
+							"h": 413,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 620,
+							"h": 413,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1291600710354837500,
+					"id_str": "1291600710354837510",
+					"indices": [
+						159,
+						182
+					],
+					"media_url": "http://pbs.twimg.com/media/EeywALxXoAYOhNL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeywALxXoAYOhNL.jpg",
+					"url": "https://t.co/ZG15Md8lyR",
+					"display_url": "pic.twitter.com/ZG15Md8lyR",
+					"expanded_url": "https://twitter.com/timeshighered/status/1291600712665882624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 620,
+							"h": 413,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 620,
+							"h": 413,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 620,
+							"h": 413,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 23602600,
+			"id_str": "23602600",
+			"name": "Times Higher Education",
+			"screen_name": "timeshighered",
+			"location": "Global",
+			"description": "Global HE news, comment & features. Publisher of the Times Higher Education World University Rankings (@THEworldunirank). For student advice, see @THEuniadvice.",
+			"url": "http://t.co/SHlBxmu5aC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/SHlBxmu5aC",
+							"expanded_url": "http://www.timeshighereducation.com",
+							"display_url": "timeshighereducation.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 301541,
+			"friends_count": 3481,
+			"listed_count": 5641,
+			"created_at": "Tue Mar 10 15:08:03 +0000 2009",
+			"favourites_count": 4978,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 118320,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1029658581434544129/TRvtevHr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1029658581434544129/TRvtevHr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/23602600/1596697768",
+			"profile_link_color": "E02200",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F7F1D2",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In a letter, leading UK scientists have set out seven principles to stamp out bullying and harassment in research, reports @annamckie.\nhttps://t.co/QF8BuCCUxG https://t.co/ZG15Md8lyR"
+	},
+	{
 		"created_at": "Fri Aug 07 09:53:34 +0000 2020",
 		"id": 1291673834714796000,
 		"id_str": "1291673834714796032",
