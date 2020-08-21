f1aa154ebee690e69257bd9d6834d3e4090d3cb4
Steven <steven.hill1@gmail.com>
2020-08-21 02:10:14 +0100

    Added 1 tweets

--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,159 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Thu Aug 20 11:23:16 +0000 2020",
+		"id": 1296393892166803500,
+		"id_str": "1296407450632032257",
+		"full_text": "I'm recruiting a Research Associate for a 5 month project to understand the value of QR funding, a joint project with the @BennettInst. Please spread the word and point interested people to https://t.co/lbAlmd35BB. @CSciPol @jonathancgrant @egwilson @AdamKamenetzky @CMManville",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BennettInst",
+					"name": "Bennett Institute for Public Policy",
+					"id": 872034768896102400,
+					"id_str": "872034768896102400",
+					"indices": [
+						122,
+						134
+					]
+				},
+				{
+					"screen_name": "CSciPol",
+					"name": "CSaP - The Centre for Science and Policy",
+					"id": 140735403,
+					"id_str": "140735403",
+					"indices": [
+						215,
+						223
+					]
+				},
+				{
+					"screen_name": "jonathancgrant",
+					"name": "Jonathan Grant",
+					"id": 564764031,
+					"id_str": "564764031",
+					"indices": [
+						224,
+						239
+					]
+				},
+				{
+					"screen_name": "egwilson",
+					"name": "ElaineWilson",
+					"id": 20764615,
+					"id_str": "20764615",
+					"indices": [
+						240,
+						249
+					]
+				},
+				{
+					"screen_name": "AdamKamenetzky",
+					"name": "Adam Kamenetzky",
+					"id": 308621548,
+					"id_str": "308621548",
+					"indices": [
+						250,
+						265
+					]
+				},
+				{
+					"screen_name": "CMManville",
+					"name": "Catriona Manville",
+					"id": 583645190,
+					"id_str": "583645190",
+					"indices": [
+						266,
+						277
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/lbAlmd35BB",
+					"expanded_url": "http://www.jobs.cam.ac.uk/job/26329/",
+					"display_url": "jobs.cam.ac.uk/job/26329/",
+					"indices": [
+						190,
+						213
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 796815535,
+			"id_str": "796815535",
+			"name": "Steven Wooding",
+			"screen_name": "drstevenwooding",
+			"location": "Cambridge, England",
+			"description": "Promoting evidence, science of science, visualising meaning, much else. RTs not endorsements. Res Fellow @CSciPol, @BennettInst @IntellForum, @Cambridge_Uni",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 390,
+			"friends_count": 72,
+			"listed_count": 10,
+			"created_at": "Sat Sep 01 20:24:39 +0000 2012",
+			"favourites_count": 110,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 820,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/854216410431721472/uuajH9Dt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/854216410431721472/uuajH9Dt_normal.jpg",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I'm recruiting a Research Associate for a 5 month project to understand the value of QR funding, a joint project with the @BennettInst. Please spread the word and point interested people to https://t.co/lbAlmd35BB. @CSciPol @jonathancgrant @egwilson @AdamKamenetzky @CMManville"
+	},
+	{
 		"created_at": "Wed Aug 19 09:39:02 +0000 2020",
 		"id": 1296018830779723800,
 		"id_str": "1296018830779723776",
