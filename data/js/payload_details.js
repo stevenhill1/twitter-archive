d887828e36d4b782eb9f4853c5108559a867d04e
Steven <steven.hill1@gmail.com>
2020-09-07 02:10:16 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6034,
-	"created_at": "Sun Sep 06 01:10:13 +0000 2020",
+	"tweets": 6035,
+	"created_at": "Mon Sep 07 01:10:15 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,131 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sun Sep 06 09:03:06 +0000 2020",
+		"id": 1302532769290035200,
+		"id_str": "1302532769290035202",
+		"full_text": "@SimonRKerridge @LizzieGadd @ersatzben I'll take that as a compliment... So, how about 48x27? 960 + 350 - 14 = 1296 is my route. The '350-14' bit uses the same principle (48 is nearly 50, so do 50 and then correct)",
+		"truncated": false,
+		"display_text_range": [
+			39,
+			214
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SimonRKerridge",
+					"name": "Simon Kerridge",
+					"id": 379704500,
+					"id_str": "379704500",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "LizzieGadd",
+					"name": "Dr Elizabeth Gadd",
+					"id": 47917199,
+					"id_str": "47917199",
+					"indices": [
+						16,
+						27
+					]
+				},
+				{
+					"screen_name": "ersatzben",
+					"name": "Ben Johnson",
+					"id": 1840757556,
+					"id_str": "1840757556",
+					"indices": [
+						28,
+						38
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1302524493609480200,
+		"in_reply_to_status_id_str": "1302524493609480193",
+		"in_reply_to_user_id": 379704500,
+		"in_reply_to_user_id_str": "379704500",
+		"in_reply_to_screen_name": "SimonRKerridge",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5022,
+			"friends_count": 748,
+			"listed_count": 178,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 92,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5910,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SimonRKerridge @LizzieGadd @ersatzben I'll take that as a compliment... So, how about 48x27? 960 + 350 - 14 = 1296 is my route. The '350-14' bit uses the same principle (48 is nearly 50, so do 50 and then correct)"
+	},
+	{
 		"created_at": "Sat Sep 05 19:52:53 +0000 2020",
 		"id": 1302333903626272800,
 		"id_str": "1302333903626272768",
