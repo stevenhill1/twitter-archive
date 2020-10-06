45c8d8eac4d01f22227f2fa9bd248626900d9391
Steven <steven.hill1@gmail.com>
2020-10-06 02:10:14 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6037,
-	"created_at": "Mon Oct 05 01:10:13 +0000 2020",
+	"tweets": 6038,
+	"created_at": "Tue Oct 06 01:10:13 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Mon Oct 05 10:34:30 +0000 2020",
+		"id": 1313051423102427100,
+		"id_str": "1313065016086724609",
+		"full_text": "We have commissioned @RANDEurope to conduct a study into perceptions of the REF 2021, starting with a survey of UK researchers. \nFind out more 👉 https://t.co/b8w3nbStut \nIf you are a researcher in the UK, have your say here 👉https://t.co/783spIhOJJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			248
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RANDEurope",
+					"name": "RAND Europe",
+					"id": 22954137,
+					"id_str": "22954137",
+					"indices": [
+						21,
+						32
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/b8w3nbStut",
+					"expanded_url": "http://orlo.uk/5sETy",
+					"display_url": "orlo.uk/5sETy",
+					"indices": [
+						145,
+						168
+					]
+				},
+				{
+					"url": "https://t.co/783spIhOJJ",
+					"expanded_url": "http://orlo.uk/InFB5",
+					"display_url": "orlo.uk/InFB5",
+					"indices": [
+						225,
+						248
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://orlo.tech\" rel=\"nofollow\">Orlo</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 823853384138637300,
+			"id_str": "823853384138637313",
+			"name": "Research England",
+			"screen_name": "ResEngland",
+			"location": "Bristol",
+			"description": "We create and sustain the conditions for a healthy, dynamic research and knowledge exchange system in England's universities. We are part of @UKRI_News.",
+			"url": "https://t.co/BI90gIR8Ar",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BI90gIR8Ar",
+							"expanded_url": "https://re.ukri.org/",
+							"display_url": "re.ukri.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9929,
+			"friends_count": 366,
+			"listed_count": 127,
+			"created_at": "Tue Jan 24 11:22:08 +0000 2017",
+			"favourites_count": 1377,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1314,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1278277299457216514/Ck9k87cr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1278277299457216514/Ck9k87cr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/823853384138637313/1570693021",
+			"profile_link_color": "ACBD37",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 31,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We have commissioned @RANDEurope to conduct a study into perceptions of the REF 2021, starting with a survey of UK researchers. \nFind out more 👉 https://t.co/b8w3nbStut \nIf you are a researcher in the UK, have your say here 👉https://t.co/783spIhOJJ"
+	},
+	{
 		"created_at": "Sun Oct 04 17:40:18 +0000 2020",
 		"id": 1312809786837270500,
 		"id_str": "1312809786837270531",
