aa0b007765d2d46194efb66a6497b203c2aef116
Steven <steven.hill1@gmail.com>
2020-10-09 02:10:15 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6038,
-	"created_at": "Tue Oct 06 01:10:13 +0000 2020",
+	"tweets": 6039,
+	"created_at": "Fri Oct 09 01:10:13 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 08 13:56:44 +0000 2020",
+		"id": 1314196667453722600,
+		"id_str": "1314203077214310400",
+		"full_text": "Postgraduate mental health programme deemed a success: £1.5m programme commissioned by Research England led to increased awareness of mental health, report finds. (£) @sophie_inge @officestudents @UKRI_News @ResEngland \n\nhttps://t.co/Kfcsm3EJP3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			244
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sophie_inge",
+					"name": "Sophie Inge",
+					"id": 69899894,
+					"id_str": "69899894",
+					"indices": [
+						167,
+						179
+					]
+				},
+				{
+					"screen_name": "officestudents",
+					"name": "The Office for Students (OfS)",
+					"id": 801389270485307400,
+					"id_str": "801389270485307392",
+					"indices": [
+						180,
+						195
+					]
+				},
+				{
+					"screen_name": "UKRI_News",
+					"name": "UK Research and Innovation",
+					"id": 732141019114930200,
+					"id_str": "732141019114930176",
+					"indices": [
+						196,
+						206
+					]
+				},
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						207,
+						218
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Kfcsm3EJP3",
+					"expanded_url": "https://www.researchprofessionalnews.com/rr-news-uk-universities-2020-10-postgraduate-mental-health-programme-deemed-a-success/",
+					"display_url": "researchprofessionalnews.com/rr-news-uk-uni…",
+					"indices": [
+						221,
+						244
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 46114448,
+			"id_str": "46114448",
+			"name": "Research Fortnight",
+			"screen_name": "ResFortnight",
+			"location": "London, UK",
+			"description": "The UK’s leading independent source of research policy news and analysis, plus funding opportunities and academic jobs. Part of @ExLibrisGroup.",
+			"url": "https://t.co/4pWI3BGSem",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4pWI3BGSem",
+							"expanded_url": "http://info.researchprofessional.com/research-fortnight/",
+							"display_url": "info.researchprofessional.com/research-fortn…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23929,
+			"friends_count": 10800,
+			"listed_count": 536,
+			"created_at": "Wed Jun 10 13:45:57 +0000 2009",
+			"favourites_count": 474,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13530,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0E3373",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/933278332317134848/0w7iZFFY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/933278332317134848/0w7iZFFY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/46114448/1511351251",
+			"profile_link_color": "003B7D",
+			"profile_sidebar_border_color": "C6E2EE",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Postgraduate mental health programme deemed a success: £1.5m programme commissioned by Research England led to increased awareness of mental health, report finds. (£) @sophie_inge @officestudents @UKRI_News @ResEngland \n\nhttps://t.co/Kfcsm3EJP3"
+	},
+	{
 		"created_at": "Mon Oct 05 10:34:30 +0000 2020",
 		"id": 1313051423102427100,
 		"id_str": "1313065016086724609",
