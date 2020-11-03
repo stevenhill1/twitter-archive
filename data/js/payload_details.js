f754cd0e9fcdb121386b0024dd6940ca69195250
Steven <steven.hill1@gmail.com>
2020-11-03 01:10:14 +0000

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6046,
-	"created_at": "Sun Nov 01 01:10:12 +0000 2020",
+	"tweets": 6049,
+	"created_at": "Tue Nov 03 01:10:13 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,555 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Mon Nov 02 19:47:07 +0000 2020",
+		"id": 1323336289278693400,
+		"id_str": "1323350946936721409",
+		"full_text": "Delighted to speak in this @AesisNet session (11.30am CET) and discuss “Who do you want to be? Developing healthy, literate and value led approaches to institutional impact profiles”. See you there! https://t.co/kzIDmfiGqO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			198
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AesisNet",
+					"name": "AESIS",
+					"id": 323199460,
+					"id_str": "323199460",
+					"indices": [
+						27,
+						36
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/kzIDmfiGqO",
+					"expanded_url": "https://twitter.com/stevenhill/status/1323234408053301249",
+					"display_url": "twitter.com/stevenhill/sta…",
+					"indices": [
+						199,
+						222
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1666048531,
+			"id_str": "1666048531",
+			"name": "Julie Bayley",
+			"screen_name": "JulieEBayley",
+			"location": "University of Lincoln",
+			"description": "Director of Research Impact Development @unilincoln; Impact literacy; Health Psychologist; PhD; ARMA Director of Qualifications; blood clot(s) veteran; Carer.",
+			"url": "https://t.co/I3W3ubsgwM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/I3W3ubsgwM",
+							"expanded_url": "https://juliebayleyblog.wordpress.com/",
+							"display_url": "juliebayleyblog.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2701,
+			"friends_count": 612,
+			"listed_count": 112,
+			"created_at": "Mon Aug 12 21:01:52 +0000 2013",
+			"favourites_count": 14945,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21170,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1052074974935220224/LRpPVGpo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1052074974935220224/LRpPVGpo_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1323234408053301200,
+		"quoted_status_id_str": "1323234408053301249",
+		"quoted_status_permalink": {
+			"url": "https://t.co/kzIDmfiGqO",
+			"expanded": "https://twitter.com/stevenhill/status/1323234408053301249",
+			"display": "twitter.com/stevenhill/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 02 12:04:02 +0000 2020",
+			"id": 1323234408053301200,
+			"id_str": "1323234408053301249",
+			"full_text": "On Wednesday I will be chairing a panel with @JulieEBayley and @BarendJR on instutional #impact strategies. Tweet me the questions you think I should ask... https://t.co/2b0d8IQ5o0",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				156
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "impact",
+						"indices": [
+							88,
+							95
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "JulieEBayley",
+						"name": "Julie Bayley",
+						"id": 1666048531,
+						"id_str": "1666048531",
+						"indices": [
+							45,
+							58
+						]
+					},
+					{
+						"screen_name": "BarendJR",
+						"name": "Barend v. d. Meulen",
+						"id": 278454776,
+						"id_str": "278454776",
+						"indices": [
+							63,
+							72
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/2b0d8IQ5o0",
+						"expanded_url": "https://twitter.com/AesisNet/status/1320748181152518145",
+						"display_url": "twitter.com/AesisNet/statu…",
+						"indices": [
+							157,
+							180
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 7731752,
+				"id_str": "7731752",
+				"name": "Steven Hill",
+				"screen_name": "stevenhill",
+				"location": "Wiltshire, UK",
+				"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+				"url": "https://t.co/pEfaOcJfMm",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/pEfaOcJfMm",
+								"expanded_url": "http://stevenhill.org.uk/",
+								"display_url": "stevenhill.org.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5081,
+				"friends_count": 755,
+				"listed_count": 178,
+				"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+				"favourites_count": 94,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5932,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+				"profile_link_color": "0000FF",
+				"profile_sidebar_border_color": "87BC44",
+				"profile_sidebar_fill_color": "E0FF92",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1320748181152518100,
+			"quoted_status_id_str": "1320748181152518145",
+			"quoted_status_permalink": {
+				"url": "https://t.co/2b0d8IQ5o0",
+				"expanded": "https://twitter.com/AesisNet/status/1320748181152518145",
+				"display": "twitter.com/AesisNet/statu…"
+			},
+			"retweet_count": 5,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Delighted to speak in this @AesisNet session (11.30am CET) and discuss “Who do you want to be? Developing healthy, literate and value led approaches to institutional impact profiles”. See you there! https://t.co/kzIDmfiGqO"
+	},
+	{
+		"created_at": "Mon Nov 02 19:06:03 +0000 2020",
+		"id": 1323340612444315600,
+		"id_str": "1323340612444315648",
+		"full_text": "Only just started reading this book, but had to share a quote from the first chapter... https://t.co/u99lSBtLiN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1323340610028376000,
+					"id_str": "1323340610028376064",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/El1zRgGXEAA1Ice.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/El1zRgGXEAA1Ice.jpg",
+					"url": "https://t.co/u99lSBtLiN",
+					"display_url": "pic.twitter.com/u99lSBtLiN",
+					"expanded_url": "https://twitter.com/stevenhill/status/1323340612444315648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1323340610028376000,
+					"id_str": "1323340610028376064",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/El1zRgGXEAA1Ice.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/El1zRgGXEAA1Ice.jpg",
+					"url": "https://t.co/u99lSBtLiN",
+					"display_url": "pic.twitter.com/u99lSBtLiN",
+					"expanded_url": "https://twitter.com/stevenhill/status/1323340612444315648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5081,
+			"friends_count": 755,
+			"listed_count": 178,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 94,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5932,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Only just started reading this book, but had to share a quote from the first chapter... https://t.co/u99lSBtLiN"
+	},
+	{
+		"created_at": "Mon Nov 02 15:54:15 +0000 2020",
+		"id": 1323292343886356500,
+		"id_str": "1323292343886356488",
+		"full_text": "This is a thoughtful and wise piece on future REF and research culture by @GemmaDerrick https://t.co/3EhmmpJK2u (£)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GemmaDerrick",
+					"name": "Gemma E Derrick",
+					"id": 475031908,
+					"id_str": "475031908",
+					"indices": [
+						74,
+						87
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/3EhmmpJK2u",
+					"expanded_url": "https://www.researchprofessional.com/0/rr/news/political-science-blog/2020/11/Using-the-REF-to-fix-research-culture-risks-backfiring.html",
+					"display_url": "researchprofessional.com/0/rr/news/poli…",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5081,
+			"friends_count": 755,
+			"listed_count": 178,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 94,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5932,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is a thoughtful and wise piece on future REF and research culture by @GemmaDerrick https://t.co/3EhmmpJK2u (£)"
+	}
+]
