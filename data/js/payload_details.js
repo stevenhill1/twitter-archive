930f9a50574457723cf74dc5b1bd82ca6c6fd4ec
Steven <steven.hill1@gmail.com>
2021-01-17 01:10:13 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6064,
-	"created_at": "Tue Jan 12 01:10:34 +0000 2021",
+	"tweets": 6066,
+	"created_at": "Sun Jan 17 01:10:12 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,316 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sat Jan 16 19:30:19 +0000 2021",
+		"id": 1350099100612644900,
+		"id_str": "1350525811070263297",
+		"full_text": "We have extended the deadline for submitting bids to our joint funding competition with @ResEngland to improve access and participation for Black, Asian and minority ethnic students in postgrad research until midday on 27 May: https://t.co/mELXkG12LV\n@UKRI_News https://t.co/i8oarrVY7j",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			261
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						88,
+						99
+					]
+				},
+				{
+					"screen_name": "UKRI_News",
+					"name": "UK Research and Innovation",
+					"id": 732141019114930200,
+					"id_str": "732141019114930176",
+					"indices": [
+						251,
+						261
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/mELXkG12LV",
+					"expanded_url": "http://bit.ly/37Evfte",
+					"display_url": "bit.ly/37Evfte",
+					"indices": [
+						227,
+						250
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1350099075656536000,
+					"id_str": "1350099075656536067",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/EryD9MgXUAMjd90.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EryD9MgXUAMjd90.jpg",
+					"url": "https://t.co/i8oarrVY7j",
+					"display_url": "pic.twitter.com/i8oarrVY7j",
+					"expanded_url": "https://twitter.com/officestudents/status/1350099100612644866/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 400,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1350099075656536000,
+					"id_str": "1350099075656536067",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/EryD9MgXUAMjd90.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EryD9MgXUAMjd90.jpg",
+					"url": "https://t.co/i8oarrVY7j",
+					"display_url": "pic.twitter.com/i8oarrVY7j",
+					"expanded_url": "https://twitter.com/officestudents/status/1350099100612644866/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 400,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 801389270485307400,
+			"id_str": "801389270485307392",
+			"name": "The Office for Students (OfS)",
+			"screen_name": "officestudents",
+			"location": "Bristol, England",
+			"description": "The independent regulator of higher education in England. Working to ensure all students have a fulfilling #HigherEducation experience.",
+			"url": "https://t.co/gFmpbnLQil",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gFmpbnLQil",
+							"expanded_url": "http://www.officeforstudents.org.uk",
+							"display_url": "officeforstudents.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15860,
+			"friends_count": 2081,
+			"listed_count": 139,
+			"created_at": "Wed Nov 23 11:37:45 +0000 2016",
+			"favourites_count": 2088,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3694,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/968767930937823232/mp5uRnVv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/968767930937823232/mp5uRnVv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/801389270485307392/1581584199",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We have extended the deadline for submitting bids to our joint funding competition with @ResEngland to improve access and participation for Black, Asian and minority ethnic students in postgrad research until midday on 27 May: https://t.co/mELXkG12LV\n@UKRI_News https://t.co/i8oarrVY7j"
+	},
+	{
+		"created_at": "Sat Jan 16 14:26:37 +0000 2021",
+		"id": 1350449384224387000,
+		"id_str": "1350449384224387076",
+		"full_text": "@jonathancgrant it definitely can be an option to pre-order kindle versions (which then just appear on device on publication day)",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			129
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jonathancgrant",
+					"name": "Jonathan Grant",
+					"id": 564764031,
+					"id_str": "564764031",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1350446982742102000,
+		"in_reply_to_status_id_str": "1350446982742102021",
+		"in_reply_to_user_id": 564764031,
+		"in_reply_to_user_id_str": "564764031",
+		"in_reply_to_screen_name": "jonathancgrant",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5161,
+			"friends_count": 753,
+			"listed_count": 175,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 94,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5951,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@jonathancgrant it definitely can be an option to pre-order kindle versions (which then just appear on device on publication day)"
+	},
+	{
 		"created_at": "Mon Jan 11 09:50:35 +0000 2021",
 		"id": 1348567975184498700,
 		"id_str": "1348567975184498689",
