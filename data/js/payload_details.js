875ca292492e702b6247a8ac5ab007205080dc9e
Steven <steven.hill1@gmail.com>
2021-02-06 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6067,
-	"created_at": "Fri Jan 22 01:10:12 +0000 2021",
+	"tweets": 6068,
+	"created_at": "Sat Feb 06 01:10:12 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_02.js
@@ -0,0 +1,354 @@
+Grailbird.data.tweets_2021_02 = 
+[
+	{
+		"created_at": "Fri Feb 05 13:08:04 +0000 2021",
+		"id": 1357671608853356500,
+		"id_str": "1357677369855737858",
+		"full_text": "Really important work coming to fruition, illustrating the commitment to support everyone in the research and innovation system👇 https://t.co/Og9numfBRB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Og9numfBRB",
+					"expanded_url": "https://twitter.com/ukri_news/status/1357632986737479683",
+					"display_url": "twitter.com/ukri_news/stat…",
+					"indices": [
+						129,
+						152
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887409583,
+			"id_str": "887409583",
+			"name": "Rory Duncan",
+			"screen_name": "ProfRoryDuncan",
+			"location": "Portobello, London, Swindon",
+			"description": "UKRI Director, Talent and Skills | Co-Director https://t.co/NfOGCOslGZ | Professor, Biophysics | *Personal account*, Work👇Rory.Duncan@ukri.org",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/NfOGCOslGZ",
+							"expanded_url": "http://esric.org",
+							"display_url": "esric.org",
+							"indices": [
+								47,
+								70
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1940,
+			"friends_count": 2387,
+			"listed_count": 31,
+			"created_at": "Wed Oct 17 19:19:07 +0000 2012",
+			"favourites_count": 3217,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3346,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1189402347329458176/H-pCxMXv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1189402347329458176/H-pCxMXv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887409583/1589001647",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1357632986737479700,
+		"quoted_status_id_str": "1357632986737479683",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Og9numfBRB",
+			"expanded": "https://twitter.com/ukri_news/status/1357632986737479683",
+			"display": "twitter.com/ukri_news/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Feb 05 10:11:42 +0000 2021",
+			"id": 1357632986737479700,
+			"id_str": "1357632986737479683",
+			"full_text": "We’re pleased to publish our action plan to implement the @TechsCommit, as a funder and employer: https://t.co/f8seLBrs00\n\nThe skills and expertise of technicians make research and innovation happen. Meet some of the brilliant #technicians in the UKRI community ⤵️ https://t.co/0oe9XOVEEf",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				264
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "technicians",
+						"indices": [
+							227,
+							239
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "TechsCommit",
+						"name": "Technician Commitment",
+						"id": 912430642067132400,
+						"id_str": "912430642067132418",
+						"indices": [
+							58,
+							70
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/f8seLBrs00",
+						"expanded_url": "https://www.ukri.org/news/ukri-champions-technical-careers-in-research-and-innovation/",
+						"display_url": "ukri.org/news/ukri-cham…",
+						"indices": [
+							98,
+							121
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1356997863876739000,
+						"id_str": "1356997863876739076",
+						"indices": [
+							265,
+							288
+						],
+						"media_url": "http://pbs.twimg.com/media/EtdHxqCXUAUMUwQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EtdHxqCXUAUMUwQ.jpg",
+						"url": "https://t.co/0oe9XOVEEf",
+						"display_url": "pic.twitter.com/0oe9XOVEEf",
+						"expanded_url": "https://twitter.com/UKRI_News/status/1357632986737479683/video/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1356997863876739000,
+						"id_str": "1356997863876739076",
+						"indices": [
+							265,
+							288
+						],
+						"media_url": "http://pbs.twimg.com/media/EtdHxqCXUAUMUwQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EtdHxqCXUAUMUwQ.jpg",
+						"url": "https://t.co/0oe9XOVEEf",
+						"display_url": "pic.twitter.com/0oe9XOVEEf",
+						"expanded_url": "https://twitter.com/UKRI_News/status/1357632986737479683/video/1",
+						"type": "video",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								2,
+								1
+							],
+							"duration_millis": 55000,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1356997863876739076/vid/720x360/zhAaeljT7b8-16xU.mp4?tag=13"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/1356997863876739076/pl/Ql1E1B2fEQiCwmId.m3u8?tag=13"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1356997863876739076/vid/1024x512/7JOlZhGr9RnEUu4m.mp4?tag=13"
+								},
+								{
+									"bitrate": 288000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1356997863876739076/vid/540x270/cQcJv7UmFaI6N20i.mp4?tag=13"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "UKRI Technician Commitment action plan published",
+							"description": "We are pleased to publish our action plan to implement the Technician Commitment as a funder and an employer.",
+							"call_to_actions": {
+								"visit_site": {
+									"url": "https://www.ukri.org/news/ukri-champions-technical-careers-in-research-and-innovation/"
+								}
+							},
+							"embeddable": true,
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 732141019114930200,
+				"id_str": "732141019114930176",
+				"name": "UK Research and Innovation",
+				"screen_name": "UKRI_News",
+				"location": "United Kingdom",
+				"description": "We support world-leading research and innovation to push the frontiers of human knowledge and generate social, cultural and economic impact. Learn more here 👇",
+				"url": "https://t.co/rbC2DHMUJc",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/rbC2DHMUJc",
+								"expanded_url": "https://www.ukri.org/about-us/",
+								"display_url": "ukri.org/about-us/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 58646,
+				"friends_count": 515,
+				"listed_count": 564,
+				"created_at": "Mon May 16 09:29:56 +0000 2016",
+				"favourites_count": 2958,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 7263,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1334784962046943232/1kllKSjW_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1334784962046943232/1kllKSjW_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/732141019114930176/1607072509",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 26,
+			"favorite_count": 52,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 5,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Really important work coming to fruition, illustrating the commitment to support everyone in the research and innovation system👇 https://t.co/Og9numfBRB"
+	}
+]
