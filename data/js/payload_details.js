e5f32f767aad5ece80c5ce0406683a6f77f39359
Steven <steven.hill1@gmail.com>
2021-03-11 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6077,
-	"created_at": "Mon Mar 01 01:10:12 +0000 2021",
+	"tweets": 6078,
+	"created_at": "Thu Mar 11 01:10:12 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_03.js
@@ -0,0 +1,109 @@
+Grailbird.data.tweets_2021_03 = 
+[
+	{
+		"created_at": "Wed Mar 10 11:20:43 +0000 2021",
+		"id": 1369275799606595600,
+		"id_str": "1369609155837894661",
+		"full_text": "Recruitment is now open for six expert panel members, including two postgraduate research students. We're looking for people with lived &amp; learnt experiences relevant to the call's aims to advise on funding decisions &amp; guide projects. Apply by 12 Apr: https://t.co/uCYu0ixwww (2/2)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			288
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uCYu0ixwww",
+					"expanded_url": "https://www.ukri.org/news/chair-appointment-and-call-for-expert-panel-applications/",
+					"display_url": "ukri.org/news/chair-app…",
+					"indices": [
+						259,
+						282
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1369275797886931000,
+		"in_reply_to_status_id_str": "1369275797886930952",
+		"in_reply_to_user_id": 823853384138637300,
+		"in_reply_to_user_id_str": "823853384138637313",
+		"in_reply_to_screen_name": "ResEngland",
+		"user": {
+			"id": 823853384138637300,
+			"id_str": "823853384138637313",
+			"name": "Research England",
+			"screen_name": "ResEngland",
+			"location": "Bristol",
+			"description": "We create and sustain the conditions for a healthy, dynamic research and knowledge exchange system in England's universities. We are part of @UKRI_News.",
+			"url": "https://t.co/BI90gIR8Ar",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BI90gIR8Ar",
+							"expanded_url": "https://re.ukri.org/",
+							"display_url": "re.ukri.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11402,
+			"friends_count": 368,
+			"listed_count": 144,
+			"created_at": "Tue Jan 24 11:22:08 +0000 2017",
+			"favourites_count": 1373,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1407,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1334787730820263937/vVcBrhrA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1334787730820263937/vVcBrhrA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/823853384138637313/1607073170",
+			"profile_link_color": "ACBD37",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 60,
+		"favorite_count": 46,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Recruitment is now open for six expert panel members, including two postgraduate research students. We're looking for people with lived &amp; learnt experiences relevant to the call's aims to advise on funding decisions &amp; guide projects. Apply by 12 Apr: https://t.co/uCYu0ixwww (2/2)"
+	}
+]
