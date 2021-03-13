f0a7c5d879bf17e7fbff58e79029dbe383d9ce47
Steven <steven.hill1@gmail.com>
2021-03-13 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6078,
-	"created_at": "Thu Mar 11 01:10:12 +0000 2021",
+	"tweets": 6079,
+	"created_at": "Sat Mar 13 01:10:12 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,348 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Fri Mar 12 18:57:12 +0000 2021",
+		"id": 1370448807331311600,
+		"id_str": "1370448807331311618",
+		"full_text": "Fascinating analysis of the REF2014 impact case studies by the @digitalsci team https://t.co/QJ3DNm5LaJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "digitalsci",
+					"name": "Digital Science",
+					"id": 144972081,
+					"id_str": "144972081",
+					"indices": [
+						63,
+						74
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QJ3DNm5LaJ",
+					"expanded_url": "https://twitter.com/digitalsci/status/1370420639304196097",
+					"display_url": "twitter.com/digitalsci/sta…",
+					"indices": [
+						80,
+						103
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5205,
+			"friends_count": 755,
+			"listed_count": 174,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 95,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5969,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1370420639304196000,
+		"quoted_status_id_str": "1370420639304196097",
+		"quoted_status_permalink": {
+			"url": "https://t.co/QJ3DNm5LaJ",
+			"expanded": "https://twitter.com/digitalsci/status/1370420639304196097",
+			"display": "twitter.com/digitalsci/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Mar 12 17:05:16 +0000 2021",
+			"id": 1370420639304196000,
+			"id_str": "1370420639304196097",
+			"full_text": "We've got a weekend treat for you! Settle in with your favourite hot drink and check out the latest long-read in our #SDGseries. Find out more about the link between the UN #SustainableDevelopmentGoals and #REF Impact Case Studies #SDG #REF2021 https://t.co/GV4qf7uCuV https://t.co/13gXX6XW88",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				268
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "SDGseries",
+						"indices": [
+							117,
+							127
+						]
+					},
+					{
+						"text": "SustainableDevelopmentGoals",
+						"indices": [
+							173,
+							201
+						]
+					},
+					{
+						"text": "REF",
+						"indices": [
+							206,
+							210
+						]
+					},
+					{
+						"text": "SDG",
+						"indices": [
+							231,
+							235
+						]
+					},
+					{
+						"text": "REF2021",
+						"indices": [
+							236,
+							244
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/GV4qf7uCuV",
+						"expanded_url": "http://ow.ly/TgVC50DXrLZ",
+						"display_url": "ow.ly/TgVC50DXrLZ",
+						"indices": [
+							245,
+							268
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1370420637630668800,
+						"id_str": "1370420637630668808",
+						"indices": [
+							269,
+							292
+						],
+						"media_url": "http://pbs.twimg.com/media/EwS2TyLXIAgbzK1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EwS2TyLXIAgbzK1.jpg",
+						"url": "https://t.co/13gXX6XW88",
+						"display_url": "pic.twitter.com/13gXX6XW88",
+						"expanded_url": "https://twitter.com/digitalsci/status/1370420639304196097/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1370420637630668800,
+						"id_str": "1370420637630668808",
+						"indices": [
+							269,
+							292
+						],
+						"media_url": "http://pbs.twimg.com/media/EwS2TyLXIAgbzK1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EwS2TyLXIAgbzK1.jpg",
+						"url": "https://t.co/13gXX6XW88",
+						"display_url": "pic.twitter.com/13gXX6XW88",
+						"expanded_url": "https://twitter.com/digitalsci/status/1370420639304196097/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 144972081,
+				"id_str": "144972081",
+				"name": "Digital Science",
+				"screen_name": "digitalsci",
+				"location": "London/Boston/Melbourne/Russia/Germany/Israel/Romania",
+				"description": "We invest in, nurture and support innovative businesses and technologies that make all parts of the research process more open, efficient and effective.",
+				"url": "http://t.co/0AuinG3pUl",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/0AuinG3pUl",
+								"expanded_url": "http://www.digital-science.com",
+								"display_url": "digital-science.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 28074,
+				"friends_count": 12075,
+				"listed_count": 852,
+				"created_at": "Mon May 17 20:12:53 +0000 2010",
+				"favourites_count": 9252,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 28532,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/458896241846276097/LgO-Xmnx_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/458896241846276097/LgO-Xmnx_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/144972081/1450185267",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Fascinating analysis of the REF2014 impact case studies by the @digitalsci team https://t.co/QJ3DNm5LaJ"
+	},
+	{
 		"created_at": "Wed Mar 10 11:20:43 +0000 2021",
 		"id": 1369275799606595600,
 		"id_str": "1369609155837894661",
