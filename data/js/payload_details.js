fb0750eec77362b5d3a73403d08b147b1d15a53d
Steven <steven.hill1@gmail.com>
2021-05-13 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6084,
-	"created_at": "Tue May 11 01:10:15 +0000 2021",
+	"tweets": 6085,
+	"created_at": "Thu May 13 01:10:12 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,318 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Wed May 12 15:37:59 +0000 2021",
+		"id": 1392504335419035600,
+		"id_str": "1392504335419035649",
+		"full_text": "Great chance for supervisors of doctoral students to contribute to the understanding of their crucial role https://t.co/YZ1xUxDkrx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YZ1xUxDkrx",
+					"expanded_url": "https://twitter.com/ukcge/status/1392107807101669385",
+					"display_url": "twitter.com/ukcge/status/1…",
+					"indices": [
+						107,
+						130
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5255,
+			"friends_count": 759,
+			"listed_count": 174,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 95,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5975,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1392107807101669400,
+		"quoted_status_id_str": "1392107807101669385",
+		"quoted_status_permalink": {
+			"url": "https://t.co/YZ1xUxDkrx",
+			"expanded": "https://twitter.com/ukcge/status/1392107807101669385",
+			"display": "twitter.com/ukcge/status/1…"
+		},
+		"quoted_status": {
+			"created_at": "Tue May 11 13:22:19 +0000 2021",
+			"id": 1392107807101669400,
+			"id_str": "1392107807101669385",
+			"full_text": "Today we've launched the UK Research Supervision Survey - a national survey giving those involved in research supervision an opportunity to share their experiences of supervising doctoral candidates.\n\nTake part here--&gt; https://t.co/BEcoJwsehs\n\n#WeArePostgrad #UKRSS https://t.co/i0ZaDOi2D1",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				268
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "WeArePostgrad",
+						"indices": [
+							247,
+							261
+						]
+					},
+					{
+						"text": "UKRSS",
+						"indices": [
+							262,
+							268
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/BEcoJwsehs",
+						"expanded_url": "https://research.shift-insight.co.uk/ukcgesm",
+						"display_url": "research.shift-insight.co.uk/ukcgesm",
+						"indices": [
+							222,
+							245
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1392107030845005800,
+						"id_str": "1392107030845005824",
+						"indices": [
+							269,
+							292
+						],
+						"media_url": "http://pbs.twimg.com/media/E1HB-AbXIAAbWfl.png",
+						"media_url_https": "https://pbs.twimg.com/media/E1HB-AbXIAAbWfl.png",
+						"url": "https://t.co/i0ZaDOi2D1",
+						"display_url": "pic.twitter.com/i0ZaDOi2D1",
+						"expanded_url": "https://twitter.com/ukcge/status/1392107807101669385/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1392107030845005800,
+						"id_str": "1392107030845005824",
+						"indices": [
+							269,
+							292
+						],
+						"media_url": "http://pbs.twimg.com/media/E1HB-AbXIAAbWfl.png",
+						"media_url_https": "https://pbs.twimg.com/media/E1HB-AbXIAAbWfl.png",
+						"url": "https://t.co/i0ZaDOi2D1",
+						"display_url": "pic.twitter.com/i0ZaDOi2D1",
+						"expanded_url": "https://twitter.com/ukcge/status/1392107807101669385/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 512,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 212181357,
+				"id_str": "212181357",
+				"name": "UKCGE",
+				"screen_name": "ukcge",
+				"location": "Lichfield, England",
+				"description": "The Voice of the Postgraduate Community.\n\nRegistered Charity No. 1061495",
+				"url": "https://t.co/U5NOTwwYnl",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/U5NOTwwYnl",
+								"expanded_url": "http://ukcge.ac.uk/about-ukcge/ukcge.aspx",
+								"display_url": "ukcge.ac.uk/about-ukcge/uk…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2616,
+				"friends_count": 2098,
+				"listed_count": 55,
+				"created_at": "Fri Nov 05 11:09:22 +0000 2010",
+				"favourites_count": 2777,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 3899,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1160180282/twitter_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1160180282/twitter_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/212181357/1620121150",
+				"profile_link_color": "00948F",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 38,
+			"favorite_count": 14,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 9,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Great chance for supervisors of doctoral students to contribute to the understanding of their crucial role https://t.co/YZ1xUxDkrx"
+	},
+	{
 		"created_at": "Mon May 10 08:54:59 +0000 2021",
 		"id": 1391678140485881900,
 		"id_str": "1391678140485881858",
