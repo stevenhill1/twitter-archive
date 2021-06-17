8a648834a57e5304251def7f62dec44402e1594e
Steven <steven.hill1@gmail.com>
2021-06-17 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6086,
-	"created_at": "Tue May 25 01:10:12 +0000 2021",
+	"tweets": 6087,
+	"created_at": "Thu Jun 17 01:10:12 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_06.js
@@ -0,0 +1,119 @@
+Grailbird.data.tweets_2021_06 = 
+[
+	{
+		"created_at": "Wed Jun 16 14:32:26 +0000 2021",
+		"id": 1405171411925274600,
+		"id_str": "1405171411925274627",
+		"full_text": "@CameronNeylon @dfflanders He gets criticised by Zuboff, but worth looking at both sides IMO",
+		"truncated": false,
+		"display_text_range": [
+			27,
+			92
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CameronNeylon",
+					"name": "CⓐmeronNeylon",
+					"id": 12984852,
+					"id_str": "12984852",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "dfflanders",
+					"name": "David F. Flanders",
+					"id": 12020532,
+					"id_str": "12020532",
+					"indices": [
+						15,
+						26
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1405170536255299600,
+		"in_reply_to_status_id_str": "1405170536255299591",
+		"in_reply_to_user_id": 7731752,
+		"in_reply_to_user_id_str": "7731752",
+		"in_reply_to_screen_name": "stevenhill",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5257,
+			"friends_count": 759,
+			"listed_count": 176,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 94,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5975,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@CameronNeylon @dfflanders He gets criticised by Zuboff, but worth looking at both sides IMO"
+	}
+]
