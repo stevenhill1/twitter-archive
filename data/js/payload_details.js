c6f2dd7dae5ca36afba7704bbae304268b2680ac
Steven <steven.hill1@gmail.com>
2021-08-07 02:10:13 +0100

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6088,
-	"created_at": "Sat Jun 26 01:10:12 +0000 2021",
+	"tweets": 6091,
+	"created_at": "Sat Aug 07 01:10:12 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,509 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Fri Aug 06 16:26:54 +0000 2021",
+		"id": 1423680095659733000,
+		"id_str": "1423682000158867458",
+		"full_text": "full text of ministerial speech on open access https://t.co/M61LNlmIXV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/M61LNlmIXV",
+					"expanded_url": "https://www.gov.uk/government/speeches/science-minister-announces-ukris-open-access-policy",
+					"display_url": "gov.uk/government/spe…",
+					"indices": [
+						47,
+						70
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1840757556,
+			"id_str": "1840757556",
+			"name": "Ben Johnson",
+			"screen_name": "ersatzben",
+			"location": "London",
+			"description": "Ministerial adviser on science, research and innovation policy. Policy not politics!   https://t.co/bsoHmcbIW6",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bsoHmcbIW6",
+							"expanded_url": "http://ersatzben.com/about/",
+							"display_url": "ersatzben.com/about/",
+							"indices": [
+								87,
+								110
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2099,
+			"friends_count": 1092,
+			"listed_count": 36,
+			"created_at": "Mon Sep 09 06:37:13 +0000 2013",
+			"favourites_count": 3059,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3569,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "200709",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/612204800239923201/DEGkoX5U_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/612204800239923201/DEGkoX5U_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1840757556/1560095810",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "B68B9E",
+			"profile_sidebar_fill_color": "200709",
+			"profile_text_color": "C96D43",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "full text of ministerial speech on open access https://t.co/M61LNlmIXV"
+	},
+	{
+		"created_at": "Fri Aug 06 15:52:43 +0000 2021",
+		"id": 1423668684342665200,
+		"id_str": "1423673398635413505",
+		"full_text": "A new communication platform for the scientific community is to receive Research England support. @science_octopus will provide a 'primary research record' for recording and appraising research as it happens, allowing rapid sharing and greater transparency\nhttps://t.co/TORjQXNQaK https://t.co/Why3jlazjq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "science_octopus",
+					"name": "Octopus",
+					"id": 1364598944366530600,
+					"id_str": "1364598944366530565",
+					"indices": [
+						98,
+						114
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/TORjQXNQaK",
+					"expanded_url": "https://www.ukri.org/news/funding-agreed-for-a-platform-that-will-change-research-culture/",
+					"display_url": "ukri.org/news/funding-a…",
+					"indices": [
+						257,
+						280
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1423668481006973000,
+					"id_str": "1423668481006972930",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/E8Hi8MhXMAIjqGQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8Hi8MhXMAIjqGQ.jpg",
+					"url": "https://t.co/Why3jlazjq",
+					"display_url": "pic.twitter.com/Why3jlazjq",
+					"expanded_url": "https://twitter.com/ResEngland/status/1423668684342665221/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1423668481006973000,
+					"id_str": "1423668481006972930",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/E8Hi8MhXMAIjqGQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8Hi8MhXMAIjqGQ.jpg",
+					"url": "https://t.co/Why3jlazjq",
+					"display_url": "pic.twitter.com/Why3jlazjq",
+					"expanded_url": "https://twitter.com/ResEngland/status/1423668684342665221/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 823853384138637300,
+			"id_str": "823853384138637313",
+			"name": "Research England",
+			"screen_name": "ResEngland",
+			"location": "Bristol",
+			"description": "We create and sustain the conditions for a healthy, dynamic research and knowledge exchange system in England's universities. We are part of @UKRI_News.",
+			"url": "https://t.co/BI90gIR8Ar",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BI90gIR8Ar",
+							"expanded_url": "https://re.ukri.org/",
+							"display_url": "re.ukri.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11921,
+			"friends_count": 367,
+			"listed_count": 156,
+			"created_at": "Tue Jan 24 11:22:08 +0000 2017",
+			"favourites_count": 1392,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1509,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1410530963956842498/aGP6f-Tb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1410530963956842498/aGP6f-Tb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/823853384138637313/1607073170",
+			"profile_link_color": "ACBD37",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A new communication platform for the scientific community is to receive Research England support. @science_octopus will provide a 'primary research record' for recording and appraising research as it happens, allowing rapid sharing and greater transparency\nhttps://t.co/TORjQXNQaK https://t.co/Why3jlazjq"
+	},
+	{
+		"created_at": "Fri Aug 06 15:07:59 +0000 2021",
+		"id": 1423657913051144200,
+		"id_str": "1423662142444744712",
+		"full_text": "Octopus is going to launch in partnership with @Jisc and @ukrepro with thanks to funding from @ResEngland https://t.co/1e4qtzZSb0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			105
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Jisc",
+					"name": "Jisc",
+					"id": 18829580,
+					"id_str": "18829580",
+					"indices": [
+						47,
+						52
+					]
+				},
+				{
+					"screen_name": "ukrepro",
+					"name": "UK Reproducibility Network",
+					"id": 1047951888635899900,
+					"id_str": "1047951888635899904",
+					"indices": [
+						57,
+						65
+					]
+				},
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						94,
+						105
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1423657894747295700,
+					"id_str": "1423657894747295748",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/E8HZT_oX0AQNiL7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8HZT_oX0AQNiL7.jpg",
+					"url": "https://t.co/1e4qtzZSb0",
+					"display_url": "pic.twitter.com/1e4qtzZSb0",
+					"expanded_url": "https://twitter.com/science_octopus/status/1423657913051144200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1151,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1423657894747295700,
+					"id_str": "1423657894747295748",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/E8HZT_oX0AQNiL7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8HZT_oX0AQNiL7.jpg",
+					"url": "https://t.co/1e4qtzZSb0",
+					"display_url": "pic.twitter.com/1e4qtzZSb0",
+					"expanded_url": "https://twitter.com/science_octopus/status/1423657913051144200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1151,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1364598944366530600,
+			"id_str": "1364598944366530565",
+			"name": "Octopus",
+			"screen_name": "science_octopus",
+			"location": "",
+			"description": "The new primary research record for science.\n\nOctopus aims to change the incentive structure for research communication.\n\nMoving to full launch Spring 2022.",
+			"url": "https://t.co/oxWcuth0mi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/oxWcuth0mi",
+							"expanded_url": "http://octopuspublishing.org",
+							"display_url": "octopuspublishing.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 172,
+			"friends_count": 1,
+			"listed_count": 1,
+			"created_at": "Wed Feb 24 15:32:19 +0000 2021",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1422831960968384512/YyKpOb0t_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1422831960968384512/YyKpOb0t_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1364598944366530565/1628064550",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22,
+		"favorite_count": 42,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Octopus is going to launch in partnership with @Jisc and @ukrepro with thanks to funding from @ResEngland https://t.co/1e4qtzZSb0"
+	}
+]
