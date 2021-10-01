44a2580186450d6b3e3a0872df4c7ba05c329f4d
Steven <steven.hill1@gmail.com>
2021-10-01 02:10:14 +0100

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6092,
-	"created_at": "Fri Sep 24 01:10:12 +0000 2021",
+	"tweets": 6095,
+	"created_at": "Fri Oct 01 01:10:12 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,531 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Thu Sep 30 17:03:01 +0000 2021",
+		"id": 1443612227995644000,
+		"id_str": "1443622424902348802",
+		"full_text": "Very sad to hear of these worrying news - if you have seen our colleague Graham Stone in the last 24 hours, please do get in touch with @gmpolice ASAP. \n\nCall +44 (0) 161 856 9769 with info. https://t.co/PAvldQTOev",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gmpolice",
+					"name": "Greater Manchester Police",
+					"id": 21857067,
+					"id_str": "21857067",
+					"indices": [
+						136,
+						145
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/PAvldQTOev",
+					"expanded_url": "https://twitter.com/gmpolice/status/1443239155283206155",
+					"display_url": "twitter.com/gmpolice/statu…",
+					"indices": [
+						191,
+						214
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1212770533869179000,
+			"id_str": "1212770533869178882",
+			"name": "COPIMproject",
+			"screen_name": "COPIMproject",
+			"location": "",
+			"description": "An international partnership building open, community-governed systems and infrastructures to support Open Access book publishing. #OAbooks #COPIM",
+			"url": "https://t.co/Ye6QstcQAh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ye6QstcQAh",
+							"expanded_url": "https://www.copim.ac.uk",
+							"display_url": "copim.ac.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1190,
+			"friends_count": 354,
+			"listed_count": 28,
+			"created_at": "Thu Jan 02 16:21:18 +0000 2020",
+			"favourites_count": 567,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1165,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227187848367005697/VGLrGNQf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227187848367005697/VGLrGNQf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1212770533869178882/1605100420",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1443239155283206100,
+		"quoted_status_id_str": "1443239155283206155",
+		"quoted_status_permalink": {
+			"url": "https://t.co/PAvldQTOev",
+			"expanded": "https://twitter.com/gmpolice/status/1443239155283206155",
+			"display": "twitter.com/gmpolice/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Sep 29 15:40:03 +0000 2021",
+			"id": 1443239155283206100,
+			"id_str": "1443239155283206155",
+			"full_text": "#MISSING Have you seen missing Graham Stone, 53? He was last seen on Waterside, #Stockport, at around 11.45am today. He has long, dark grey hair and a long, grey beard. He was last seen wearing a bright orange/red coat, grey trousers and brown boots. Call 0161 856 9769 with info. https://t.co/CI8gtgi5a4",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MISSING",
+						"indices": [
+							0,
+							8
+						]
+					},
+					{
+						"text": "Stockport",
+						"indices": [
+							80,
+							90
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1443239144851968000,
+						"id_str": "1443239144851968000",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/FAdqWwrXEAAE9Sv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FAdqWwrXEAAE9Sv.jpg",
+						"url": "https://t.co/CI8gtgi5a4",
+						"display_url": "pic.twitter.com/CI8gtgi5a4",
+						"expanded_url": "https://twitter.com/gmpolice/status/1443239155283206155/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1134,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 642,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1489,
+								"h": 1576,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1443239144851968000,
+						"id_str": "1443239144851968000",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/FAdqWwrXEAAE9Sv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FAdqWwrXEAAE9Sv.jpg",
+						"url": "https://t.co/CI8gtgi5a4",
+						"display_url": "pic.twitter.com/CI8gtgi5a4",
+						"expanded_url": "https://twitter.com/gmpolice/status/1443239155283206155/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1134,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 642,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1489,
+								"h": 1576,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://crowdcontrolhq.com/\" rel=\"nofollow\">CrowdControlHQ</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 21857067,
+				"id_str": "21857067",
+				"name": "Greater Manchester Police",
+				"screen_name": "gmpolice",
+				"location": "Greater Manchester",
+				"description": "Greater Manchester Police's feed is monitored Mon-Fri but is not for reporting crimes. Emergency: call 999. Non-emergency: call 101, livechat/online report form",
+				"url": "http://t.co/dT3SiDCq7N",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/dT3SiDCq7N",
+								"expanded_url": "http://www.gmp.police.uk/",
+								"display_url": "gmp.police.uk",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 588443,
+				"friends_count": 582,
+				"listed_count": 1994,
+				"created_at": "Wed Feb 25 11:43:10 +0000 2009",
+				"favourites_count": 3273,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 58919,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "393939",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1384047568120737802/fF7NqpYN_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1384047568120737802/fF7NqpYN_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/21857067/1618817640",
+				"profile_link_color": "393939",
+				"profile_sidebar_border_color": "333333",
+				"profile_sidebar_fill_color": "CBE3F1",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 166,
+			"favorite_count": 29,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 9,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Very sad to hear of these worrying news - if you have seen our colleague Graham Stone in the last 24 hours, please do get in touch with @gmpolice ASAP. \n\nCall +44 (0) 161 856 9769 with info. https://t.co/PAvldQTOev"
+	},
+	{
+		"created_at": "Thu Sep 30 11:02:33 +0000 2021",
+		"id": 1443520619946127400,
+		"id_str": "1443531710294110211",
+		"full_text": "Today we have published overall recurrent budgets for the HE sector for research and knowledge exchange funding for the 2021-22 academic year: \n\nhttps://t.co/x3PHo0yHKa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/x3PHo0yHKa",
+					"expanded_url": "https://www.ukri.org/news/2-5-billion-investment-to-support-governments-rd-ambitions/",
+					"display_url": "ukri.org/news/2-5-billi…",
+					"indices": [
+						145,
+						168
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 823853384138637300,
+			"id_str": "823853384138637313",
+			"name": "Research England",
+			"screen_name": "ResEngland",
+			"location": "Bristol",
+			"description": "We create and sustain the conditions for a healthy, dynamic research and knowledge exchange system in England's universities. We are part of @UKRI_News.",
+			"url": "https://t.co/BI90gIR8Ar",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BI90gIR8Ar",
+							"expanded_url": "https://re.ukri.org/",
+							"display_url": "re.ukri.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12050,
+			"friends_count": 367,
+			"listed_count": 157,
+			"created_at": "Tue Jan 24 11:22:08 +0000 2017",
+			"favourites_count": 1392,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1522,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1410530963956842498/aGP6f-Tb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1410530963956842498/aGP6f-Tb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/823853384138637313/1607073170",
+			"profile_link_color": "ACBD37",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 17,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today we have published overall recurrent budgets for the HE sector for research and knowledge exchange funding for the 2021-22 academic year: \n\nhttps://t.co/x3PHo0yHKa"
+	},
+	{
+		"created_at": "Thu Sep 30 09:53:05 +0000 2021",
+		"id": 1443510510880796700,
+		"id_str": "1443514224735662080",
+		"full_text": "@stevenhill @ResEngland “£30 million into RDP through to provide additional support to their postgraduate research students (PGR) students to respond to and recover from the challenges of the COVID-19 pandemic” is especially welcome!",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			233
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stevenhill",
+					"name": "Steven Hill",
+					"id": 7731752,
+					"id_str": "7731752",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						12,
+						23
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1443487280715419600,
+		"in_reply_to_status_id_str": "1443487280715419648",
+		"in_reply_to_user_id": 7731752,
+		"in_reply_to_user_id_str": "7731752",
+		"in_reply_to_screen_name": "stevenhill",
+		"user": {
+			"id": 188014874,
+			"id_str": "188014874",
+			"name": "Owen Gower",
+			"screen_name": "dr_og",
+			"location": "Lichfield",
+			"description": "Director, UK Council for Graduate Education @ukcge ...all views my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 975,
+			"friends_count": 713,
+			"listed_count": 7,
+			"created_at": "Tue Sep 07 18:36:24 +0000 2010",
+			"favourites_count": 2367,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1512,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1113014186903302145/0i2N95PI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1113014186903302145/0i2N95PI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/188014874/1620747983",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@stevenhill @ResEngland “£30 million into RDP through to provide additional support to their postgraduate research students (PGR) students to respond to and recover from the challenges of the COVID-19 pandemic” is especially welcome!"
+	},
+	{
 		"created_at": "Thu Sep 23 10:04:03 +0000 2021",
 		"id": 1440979308907012000,
 		"id_str": "1440980272170905602",
