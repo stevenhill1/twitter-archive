40f0347c024beac422f86fe44192620480ae15a8
Steven <steven.hill1@gmail.com>
2022-01-22 01:10:12 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6122,
-	"created_at": "Tue Jan 18 01:10:12 +0000 2022",
+	"tweets": 6123,
+	"created_at": "Sat Jan 22 01:10:12 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Fri Jan 21 16:41:52 +0000 2022",
+		"id": 1484566928190496800,
+		"id_str": "1484566928190496784",
+		"full_text": "This short thread on the relationship between evidence and policy choices is well worth a read https://t.co/QBfLn2Fjp4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QBfLn2Fjp4",
+					"expanded_url": "https://twitter.com/justinparkhurst/status/1484484657454497796",
+					"display_url": "twitter.com/justinparkhurs…",
+					"indices": [
+						95,
+						118
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5284,
+			"friends_count": 773,
+			"listed_count": 173,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 102,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6020,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1484484657454497800,
+		"quoted_status_id_str": "1484484657454497796",
+		"quoted_status_permalink": {
+			"url": "https://t.co/QBfLn2Fjp4",
+			"expanded": "https://twitter.com/justinparkhurst/status/1484484657454497796",
+			"display": "twitter.com/justinparkhurs…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jan 21 11:14:57 +0000 2022",
+			"id": 1484484657454497800,
+			"id_str": "1484484657454497796",
+			"full_text": "A 🧵 on why I don't think 'following the science' / 'not following the science' is a good way to describe or criticise the government's covid policies\n\n#evidencebasedpolicy\n#Evidence\n#COVID19 \n#followingthescience",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				212
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "evidencebasedpolicy",
+						"indices": [
+							151,
+							171
+						]
+					},
+					{
+						"text": "Evidence",
+						"indices": [
+							172,
+							181
+						]
+					},
+					{
+						"text": "COVID19",
+						"indices": [
+							182,
+							190
+						]
+					},
+					{
+						"text": "followingthescience",
+						"indices": [
+							192,
+							212
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 162074338,
+				"id_str": "162074338",
+				"name": "Justin Parkhurst",
+				"screen_name": "justinparkhurst",
+				"location": "London, UK",
+				"description": "Assoc. Prof @LSEHealthPolicy. Author 'The Politics of Evidence' (free ebook: https://t.co/LKcONBQiwe). Global health & development, evidence & policy. 🇺🇸🇬🇧🌍",
+				"url": "https://t.co/hmEhnXLNA8",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/hmEhnXLNA8",
+								"expanded_url": "https://www.researchgate.net/profile/Justin_Parkhurst",
+								"display_url": "researchgate.net/profile/Justin…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/LKcONBQiwe",
+								"expanded_url": "http://bit.ly/2KeLJO0",
+								"display_url": "bit.ly/2KeLJO0",
+								"indices": [
+									77,
+									100
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1658,
+				"friends_count": 877,
+				"listed_count": 38,
+				"created_at": "Fri Jul 02 15:29:58 +0000 2010",
+				"favourites_count": 14115,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 7830,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1387329944439623684/ZFarIP_7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1387329944439623684/ZFarIP_7_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/162074338/1605807779",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 31,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This short thread on the relationship between evidence and policy choices is well worth a read https://t.co/QBfLn2Fjp4"
+	},
+	{
 		"created_at": "Mon Jan 17 19:21:12 +0000 2022",
 		"id": 1483157477109686300,
 		"id_str": "1483157477109686278",
