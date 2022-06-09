2fe20f2fa4efe9f8d9686347307c7578c30e147e
Steven <steven.hill1@gmail.com>
2022-06-09 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6131,
-	"created_at": "Fri Jun 03 01:10:12 +0000 2022",
+	"tweets": 6132,
+	"created_at": "Thu Jun 09 01:10:12 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,124 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 08 14:27:44 +0000 2022",
+		"id": 1534538908792504300,
+		"id_str": "1534542698102894594",
+		"full_text": "\"A holistic approach, while not easy, will give us an opportunity to connect work most effectively.\" @MelanieWelham, Executive Champion for People, Culture &amp; Talent, shares our approach to build a resilient system where everyone can contribute and benefit: https://t.co/RJDuQyQZSI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			284
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MelanieWelham",
+					"name": "Melanie Welham",
+					"id": 311603420,
+					"id_str": "311603420",
+					"indices": [
+						101,
+						115
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/RJDuQyQZSI",
+					"expanded_url": "https://orlo.uk/i7l9Z",
+					"display_url": "orlo.uk/i7l9Z",
+					"indices": [
+						261,
+						284
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://orlo.tech\" rel=\"nofollow\">Orlo</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 732141019114930200,
+			"id_str": "732141019114930176",
+			"name": "UK Research and Innovation",
+			"screen_name": "UKRI_News",
+			"location": "United Kingdom",
+			"description": "We support world-leading research and innovation to push the frontiers of human knowledge and generate social, cultural and economic impact. Learn more here 👇",
+			"url": "https://t.co/90MfgtCRow",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/90MfgtCRow",
+							"expanded_url": "https://www.ukri.org/about-us/",
+							"display_url": "ukri.org/about-us/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 78044,
+			"friends_count": 587,
+			"listed_count": 729,
+			"created_at": "Mon May 16 09:29:56 +0000 2016",
+			"favourites_count": 5016,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 10672,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1531931716868243456/X_BdS8yw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1531931716868243456/X_BdS8yw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/732141019114930176/1638780955",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"A holistic approach, while not easy, will give us an opportunity to connect work most effectively.\" @MelanieWelham, Executive Champion for People, Culture &amp; Talent, shares our approach to build a resilient system where everyone can contribute and benefit: https://t.co/RJDuQyQZSI"
+	},
+	{
 		"created_at": "Thu Jun 02 07:45:38 +0000 2022",
 		"id": 1532267179143581700,
 		"id_str": "1532267179143581696",
