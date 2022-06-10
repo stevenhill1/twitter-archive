78b7249c8c95ecc2ecbc7c6cdfc177458f4a9d69
Steven <steven.hill1@gmail.com>
2022-06-10 02:10:12 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6132,
-	"created_at": "Thu Jun 09 01:10:12 +0000 2022",
+	"tweets": 6133,
+	"created_at": "Fri Jun 10 01:10:12 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 09 17:19:55 +0000 2022",
+		"id": 1534948416073105400,
+		"id_str": "1534948416073105408",
+		"full_text": "Powerful and important statement https://t.co/IYh1R5hlUA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			32
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IYh1R5hlUA",
+					"expanded_url": "https://twitter.com/Nature/status/1534803230382858241",
+					"display_url": "twitter.com/Nature/status/…",
+					"indices": [
+						33,
+						56
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5318,
+			"friends_count": 789,
+			"listed_count": 172,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 106,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6028,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1534803230382858200,
+		"quoted_status_id_str": "1534803230382858241",
+		"quoted_status_permalink": {
+			"url": "https://t.co/IYh1R5hlUA",
+			"expanded": "https://twitter.com/Nature/status/1534803230382858241",
+			"display": "twitter.com/Nature/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Jun 09 07:43:00 +0000 2022",
+			"id": 1534803230382858200,
+			"id_str": "1534803230382858241",
+			"full_text": "Science must overcome its racist legacy. Read Nature guest editors Melissa Nobles, Chad Womack, Ambroise Wonkam and Elizabeth Wathuti https://t.co/czD9qhDjO4",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				157
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/czD9qhDjO4",
+						"expanded_url": "https://go.nature.com/3aJCVzP",
+						"display_url": "go.nature.com/3aJCVzP",
+						"indices": [
+							134,
+							157
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://www.echobox.com\" rel=\"nofollow\">Echobox</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15862891,
+				"id_str": "15862891",
+				"name": "nature",
+				"screen_name": "Nature",
+				"location": "Worldwide",
+				"description": "Research, News, and Commentary from Nature, the international journal of science. \nFor daily science news, get Nature Briefing: https://t.co/wGmQlQ8a4D",
+				"url": "https://t.co/r17cjcVLWg",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/r17cjcVLWg",
+								"expanded_url": "http://www.nature.com",
+								"display_url": "nature.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/wGmQlQ8a4D",
+								"expanded_url": "http://go.nature.com/naturebriefing",
+								"display_url": "go.nature.com/naturebriefing",
+								"indices": [
+									128,
+									151
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 2328105,
+				"friends_count": 3722,
+				"listed_count": 21018,
+				"created_at": "Fri Aug 15 13:39:58 +0000 2008",
+				"favourites_count": 1542,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 69860,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "A61919",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1393206230152327170/QnzohDIu_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1393206230152327170/QnzohDIu_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15862891/1621000867",
+				"profile_link_color": "DF1F1F",
+				"profile_sidebar_border_color": "D1D1D1",
+				"profile_sidebar_fill_color": "F0F0F0",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 19,
+			"favorite_count": 38,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Powerful and important statement https://t.co/IYh1R5hlUA"
+	},
+	{
 		"created_at": "Wed Jun 08 14:27:44 +0000 2022",
 		"id": 1534538908792504300,
 		"id_str": "1534542698102894594",
