e3daa5a199acb0458941bf92beac65d36a1f9a6c
Steven <steven.hill1@gmail.com>
2022-06-18 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6138,
-	"created_at": "Fri Jun 17 01:10:12 +0000 2022",
+	"tweets": 6139,
+	"created_at": "Sat Jun 18 01:10:12 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,128 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 17 18:06:01 +0000 2022",
+		"id": 1537813914334920700,
+		"id_str": "1537859122347098113",
+		"full_text": "…&amp; here are the research roles. @LittleLizS &amp; I are looking to add a mix of 5 research associates/research assistants in our team. If you’d like work in a small interdisciplinary team @Cambridge_Uni to make a difference to #ResearchCulture take a look:\n\nhttps://t.co/Tl7qT8vDYU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			285
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ResearchCulture",
+					"indices": [
+						231,
+						247
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LittleLizS",
+					"name": "Liz Simmonds",
+					"id": 28098554,
+					"id_str": "28098554",
+					"indices": [
+						36,
+						47
+					]
+				},
+				{
+					"screen_name": "Cambridge_Uni",
+					"name": "Cambridge University",
+					"id": 33474655,
+					"id_str": "33474655",
+					"indices": [
+						192,
+						206
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Tl7qT8vDYU",
+					"expanded_url": "https://www.jobs.cam.ac.uk/job/35409/",
+					"display_url": "jobs.cam.ac.uk/job/35409/",
+					"indices": [
+						262,
+						285
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 796815535,
+			"id_str": "796815535",
+			"name": "Steven Wooding",
+			"screen_name": "drstevenwooding",
+			"location": "Cambridge, England",
+			"description": "Promoting evidence, science of science, visualising meaning, much else. RTs not endorsements. Res Fellow @CSciPol, @BennettInst @IntellForum, @Cambridge_Uni",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 410,
+			"friends_count": 87,
+			"listed_count": 10,
+			"created_at": "Sat Sep 01 20:24:39 +0000 2012",
+			"favourites_count": 186,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 959,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/854216410431721472/uuajH9Dt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/854216410431721472/uuajH9Dt_normal.jpg",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "…&amp; here are the research roles. @LittleLizS &amp; I are looking to add a mix of 5 research associates/research assistants in our team. If you’d like work in a small interdisciplinary team @Cambridge_Uni to make a difference to #ResearchCulture take a look:\n\nhttps://t.co/Tl7qT8vDYU"
+	},
+	{
 		"created_at": "Thu Jun 16 10:28:02 +0000 2022",
 		"id": 1537354640152572000,
 		"id_str": "1537381480716029955",
