37ef9bc256dd89992c2c1f56f547b3e71061c3f8
Steven <steven.hill1@gmail.com>
2022-11-05 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6164,
-	"created_at": "Fri Oct 28 01:10:12 +0000 2022",
+	"tweets": 6165,
+	"created_at": "Sat Nov 05 01:10:12 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_11.js
@@ -0,0 +1,129 @@
+Grailbird.data.tweets_2022_11 = 
+[
+	{
+		"created_at": "Fri Nov 04 08:41:27 +0000 2022",
+		"id": 1588451345052455000,
+		"id_str": "1588451345052454914",
+		"full_text": "@qutiifoundation @ResEngland @NERCscience would be best placed to answer this question",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "qutiifoundation",
+					"name": "QuTii",
+					"id": 1579484439415033900,
+					"id_str": "1579484439415033857",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						17,
+						28
+					]
+				},
+				{
+					"screen_name": "NERCscience",
+					"name": "Natural Environment Research Council",
+					"id": 104849636,
+					"id_str": "104849636",
+					"indices": [
+						29,
+						41
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1588425435637063700,
+		"in_reply_to_status_id_str": "1588425435637063681",
+		"in_reply_to_user_id": 1579484439415033900,
+		"in_reply_to_user_id_str": "1579484439415033857",
+		"in_reply_to_screen_name": "qutiifoundation",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5345,
+			"friends_count": 803,
+			"listed_count": 171,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 104,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6066,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@qutiifoundation @ResEngland @NERCscience would be best placed to answer this question"
+	}
+]
