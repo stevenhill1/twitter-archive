978fa531066659f85a149261d7a14f1decf35120
Steven <steven.hill1@gmail.com>
2022-11-15 01:10:18 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6166,
-	"created_at": "Fri Nov 11 01:10:12 +0000 2022",
+	"tweets": 6167,
+	"created_at": "Tue Nov 15 01:10:16 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,264 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Mon Nov 14 19:17:12 +0000 2022",
+		"id": 1592235215333769200,
+		"id_str": "1592235215333769219",
+		"full_text": "Proud of the @ResEngland investment in this research through the Expanding Excelence in England fund https://t.co/ym5kBslBmt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						13,
+						24
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ym5kBslBmt",
+					"expanded_url": "https://twitter.com/MarcusduSautoy/status/1592160587114770439",
+					"display_url": "twitter.com/MarcusduSautoy…",
+					"indices": [
+						101,
+						124
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5330,
+			"friends_count": 801,
+			"listed_count": 171,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 104,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6069,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1592160587114770400,
+		"quoted_status_id_str": "1592160587114770439",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ym5kBslBmt",
+			"expanded": "https://twitter.com/MarcusduSautoy/status/1592160587114770439",
+			"display": "twitter.com/MarcusduSautoy…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 14 14:20:39 +0000 2022",
+			"id": 1592160587114770400,
+			"id_str": "1592160587114770439",
+			"full_text": "Do proofs have a musical quality to them? This was such a fascinating project to work on with @EHowardComposer Find out more about our collaboration @RNCMPRiSM https://t.co/srJ1bLDuH3",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				159
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "EHowardComposer",
+						"name": "Emily Howard",
+						"id": 576200424,
+						"id_str": "576200424",
+						"indices": [
+							94,
+							110
+						]
+					},
+					{
+						"screen_name": "RNCMPRiSM",
+						"name": "RNCM PRiSM",
+						"id": 1224343332001894400,
+						"id_str": "1224343332001894400",
+						"indices": [
+							149,
+							159
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/srJ1bLDuH3",
+						"expanded_url": "https://twitter.com/RNCMPRiSM/status/1592128951404617728",
+						"display_url": "twitter.com/RNCMPRiSM/stat…",
+						"indices": [
+							160,
+							183
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 22272053,
+				"id_str": "22272053",
+				"name": "Marcus du Sautoy",
+				"screen_name": "MarcusduSautoy",
+				"location": "",
+				"description": "Simonyi Professor for the Public Understanding of Science and Professor of Mathematics. Author of Thinking Better (Fourth Estate), out August 2021.",
+				"url": "https://t.co/QHruCLN3QI",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/QHruCLN3QI",
+								"expanded_url": "https://www.simonyi.ox.ac.uk/",
+								"display_url": "simonyi.ox.ac.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 54429,
+				"friends_count": 407,
+				"listed_count": 1666,
+				"created_at": "Sat Feb 28 16:47:51 +0000 2009",
+				"favourites_count": 431,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 6350,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1422127296182464512/0K4SLB1k_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1422127296182464512/0K4SLB1k_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/22272053/1634632604",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "BDDCAD",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1592128951404617700,
+			"quoted_status_id_str": "1592128951404617728",
+			"quoted_status_permalink": {
+				"url": "https://t.co/srJ1bLDuH3",
+				"expanded": "https://twitter.com/RNCMPRiSM/status/1592128951404617728",
+				"display": "twitter.com/RNCMPRiSM/stat…"
+			},
+			"retweet_count": 3,
+			"favorite_count": 8,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Proud of the @ResEngland investment in this research through the Expanding Excelence in England fund https://t.co/ym5kBslBmt"
+	},
+	{
 		"created_at": "Thu Nov 10 19:27:21 +0000 2022",
 		"id": 1590715945722396700,
 		"id_str": "1590788217263034379",
