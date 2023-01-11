503336293f031e4e74622e417a02bbb8509a0474
Steven <steven.hill1@gmail.com>
2023-01-11 01:10:14 +0000

    Added 1 tweets

--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Tue Jan 10 15:12:16 +0000 2023",
+		"id": 1612829686832275500,
+		"id_str": "1612829686832275456",
+		"full_text": "Interesting. Contrasts with the findings of @OhidYaqub on UK system https://t.co/uMXoCAfmij \"...conditional on REF-inclusion, research delivers impact with largely the same orientation as envisaged at the outset of their projects\" https://t.co/ukG6jrQT5q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OhidYaqub",
+					"name": "Ohid Yaqub",
+					"id": 536156905,
+					"id_str": "536156905",
+					"indices": [
+						44,
+						54
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/uMXoCAfmij",
+					"expanded_url": "https://osf.io/preprints/socarxiv/qw873/",
+					"display_url": "osf.io/preprints/soca…",
+					"indices": [
+						68,
+						91
+					]
+				},
+				{
+					"url": "https://t.co/ukG6jrQT5q",
+					"expanded_url": "https://twitter.com/profmarkreed/status/1612725646261899267",
+					"display_url": "twitter.com/profmarkreed/s…",
+					"indices": [
+						231,
+						254
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5314,
+			"friends_count": 805,
+			"listed_count": 171,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 102,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6083,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1612725646261899300,
+		"quoted_status_id_str": "1612725646261899267",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ukG6jrQT5q",
+			"expanded": "https://twitter.com/profmarkreed/status/1612725646261899267",
+			"display": "twitter.com/profmarkreed/s…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jan 10 08:18:51 +0000 2023",
+			"id": 1612725646261899300,
+			"id_str": "1612725646261899267",
+			"full_text": "New study shows that projects funded by @NSF consistently fail to deliver the impacts they set out to achieve - except for advantaged groups who recieve more impacts than originally planned https://t.co/ZblYIWXviO",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				213
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "NSF",
+						"name": "National Science Foundation",
+						"id": 16245822,
+						"id_str": "16245822",
+						"indices": [
+							40,
+							44
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/ZblYIWXviO",
+						"expanded_url": "https://academic.oup.com/rev/advance-article/doi/10.1093/reseval/rvac046/6972169?login=false",
+						"display_url": "academic.oup.com/rev/advance-ar…",
+						"indices": [
+							190,
+							213
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 19347472,
+				"id_str": "19347472",
+				"name": "Mark Reed",
+				"screen_name": "profmarkreed",
+				"location": "United Kingdom",
+				"description": "Director, Thriving Natural Capital Challenge Centre | Professor of Rural Entrepreneurship @SRUC | Impact training @fasttrackimpact (he/him). Get my new book:",
+				"url": "https://t.co/m60ZtsaNLR",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/m60ZtsaNLR",
+								"expanded_url": "https://www.fasttrackimpact.com/impactculture",
+								"display_url": "fasttrackimpact.com/impactculture",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 9636,
+				"friends_count": 443,
+				"listed_count": 197,
+				"created_at": "Thu Jan 22 16:54:23 +0000 2009",
+				"favourites_count": 9902,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 16702,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "20201E",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1333704556816068611/AOoHkPWi_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1333704556816068611/AOoHkPWi_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/19347472/1648125464",
+				"profile_link_color": "4A913C",
+				"profile_sidebar_border_color": "C8A867",
+				"profile_sidebar_fill_color": "20201E",
+				"profile_text_color": "897856",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Interesting. Contrasts with the findings of @OhidYaqub on UK system https://t.co/uMXoCAfmij \"...conditional on REF-inclusion, research delivers impact with largely the same orientation as envisaged at the outset of their projects\" https://t.co/ukG6jrQT5q"
+	},
+	{
 		"created_at": "Thu Jan 05 16:34:20 +0000 2023",
 		"id": 1611038397434396700,
 		"id_str": "1611038397434396672",
