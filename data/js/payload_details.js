44b2b8ea7221372b62bb508d0a413be1bbaf355e
Steven <steven.hill1@gmail.com>
2023-01-20 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6183,
-	"created_at": "Sat Jan 14 01:10:12 +0000 2023",
+	"tweets": 6184,
+	"created_at": "Fri Jan 20 01:10:12 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,121 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Thu Jan 19 09:22:19 +0000 2023",
+		"id": 1615998762379509800,
+		"id_str": "1616003108949082113",
+		"full_text": "Great to see this pre-announced! Such a good opportunity for interdisciplinary research. More details to follow later in the year but fantastic to have been working with @samaspinall on secondment to UKRI and colleagues across UKRI to get us this far!\n\nhttps://t.co/72zG5yqqff",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "samaspinall",
+					"name": "Samantha Aspinall 💙",
+					"id": 20233236,
+					"id_str": "20233236",
+					"indices": [
+						170,
+						182
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/72zG5yqqff",
+					"expanded_url": "https://www.ukri.org/opportunity/ukri-cross-research-council-responsive-mode-pilot-scheme/",
+					"display_url": "ukri.org/opportunity/uk…",
+					"indices": [
+						253,
+						276
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 28188415,
+			"id_str": "28188415",
+			"name": "kirsty grainger",
+			"screen_name": "kirstygrainger",
+			"location": "Wiltshire",
+			"description": "Optimist. Research Funding/Strategy/Policy Leadership. She/her. @kirstygrainger@mastodon.social\nhttps://t.co/cJ3DqxVkYE…",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/cJ3DqxVkYE",
+							"expanded_url": "http://linkedin.com/in/kirsty-grai",
+							"display_url": "linkedin.com/in/kirsty-grai",
+							"indices": [
+								96,
+								119
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2615,
+			"friends_count": 625,
+			"listed_count": 20,
+			"created_at": "Wed Apr 01 20:33:25 +0000 2009",
+			"favourites_count": 10469,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9972,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "153B5C",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283078317491576832/7sE_zA32_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283078317491576832/7sE_zA32_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/28188415/1665762793",
+			"profile_link_color": "492754",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EADEAA",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 23,
+		"favorite_count": 56,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Great to see this pre-announced! Such a good opportunity for interdisciplinary research. More details to follow later in the year but fantastic to have been working with @samaspinall on secondment to UKRI and colleagues across UKRI to get us this far!\n\nhttps://t.co/72zG5yqqff"
+	},
+	{
 		"created_at": "Fri Jan 13 15:51:33 +0000 2023",
 		"id": 1613921313034207200,
 		"id_str": "1613926732620333056",
