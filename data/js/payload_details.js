432a5f6bc35fbe7ef1236a482b42e1bc11bbc23b
Steven <steven.hill1@gmail.com>
2023-03-03 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6189,
-	"created_at": "Thu Feb 09 01:10:12 +0000 2023",
+	"tweets": 6190,
+	"created_at": "Fri Mar 03 01:10:12 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_03.js
@@ -0,0 +1,219 @@
+Grailbird.data.tweets_2023_03 = 
+[
+	{
+		"created_at": "Thu Mar 02 19:14:54 +0000 2023",
+		"id": 1631266854584090600,
+		"id_str": "1631372526596005908",
+		"full_text": "Many thanks to @Revathykri, Dominic Chiwenga and @claire_wilson2  for joining us yesterday to share their experiences with our @ResEngland funders @stevenhill and Alexa Bishop. \n\nIt was great to hear about first-hand experiences and discuss plans for our launch to the sector! https://t.co/kmPdj1DkG6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Revathykri",
+					"name": "RevathyKrishnamurthi",
+					"id": 1386630715169775600,
+					"id_str": "1386630715169775617",
+					"indices": [
+						15,
+						26
+					]
+				},
+				{
+					"screen_name": "claire_wilson2",
+					"name": "Claire Wilson",
+					"id": 376153434,
+					"id_str": "376153434",
+					"indices": [
+						49,
+						64
+					]
+				},
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						127,
+						138
+					]
+				},
+				{
+					"screen_name": "stevenhill",
+					"name": "Steven Hill",
+					"id": 7731752,
+					"id_str": "7731752",
+					"indices": [
+						147,
+						158
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1631262857621827600,
+					"id_str": "1631262857621827586",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FqNo7RvWcAIZxPn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqNo7RvWcAIZxPn.jpg",
+					"url": "https://t.co/kmPdj1DkG6",
+					"display_url": "pic.twitter.com/kmPdj1DkG6",
+					"expanded_url": "https://twitter.com/ProsperPostdoc/status/1631266854584090624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1631262857621827600,
+					"id_str": "1631262857621827586",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FqNo7RvWcAIZxPn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqNo7RvWcAIZxPn.jpg",
+					"url": "https://t.co/kmPdj1DkG6",
+					"display_url": "pic.twitter.com/kmPdj1DkG6",
+					"expanded_url": "https://twitter.com/ProsperPostdoc/status/1631266854584090624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1229418499115802600,
+			"id_str": "1229418499115802625",
+			"name": "Prosper",
+			"screen_name": "ProsperPostdoc",
+			"location": "UK",
+			"description": "Unlocking postdoc career potential. Part of @LivUniAcademy at @livuni, in partnership with @OfficialUoM and @LancasterUni, supported by @UKRI_News",
+			"url": "https://t.co/vWhKH2e0Wc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vWhKH2e0Wc",
+							"expanded_url": "http://liv.ac.uk/prosperproject",
+							"display_url": "liv.ac.uk/prosperproject",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 996,
+			"friends_count": 348,
+			"listed_count": 4,
+			"created_at": "Mon Feb 17 14:53:36 +0000 2020",
+			"favourites_count": 811,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 934,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1272525205227933697/njzawPfB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1272525205227933697/njzawPfB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1229418499115802625/1592228627",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Many thanks to @Revathykri, Dominic Chiwenga and @claire_wilson2  for joining us yesterday to share their experiences with our @ResEngland funders @stevenhill and Alexa Bishop. \n\nIt was great to hear about first-hand experiences and discuss plans for our launch to the sector! https://t.co/kmPdj1DkG6"
+	}
+]
