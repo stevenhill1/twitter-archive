deb6685c4d5c20422898c8145a41327b2cc1a72f
Steven <steven.hill1@gmail.com>
2023-03-12 01:10:13 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6190,
-	"created_at": "Fri Mar 03 01:10:12 +0000 2023",
+	"tweets": 6191,
+	"created_at": "Sun Mar 12 01:10:12 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Sat Mar 11 08:22:51 +0000 2023",
+		"id": 1634315769562509300,
+		"id_str": "1634469922653761539",
+		"full_text": "Hello BBC Sport. Decide if you are a wing of the government or not? I realise that this may disqualify me from ever again commentating on the mountain biking on the Commonwealth Games, but it’s a hill (literally) I am prepared (not literally) to die on. Also, I was awful at it.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16237253,
+			"id_str": "16237253",
+			"name": "Ned Boulting 🏳️‍⚧️",
+			"screen_name": "nedboulting",
+			"location": "Lewisham",
+			"description": "Back in a bit",
+			"url": "https://t.co/009iczKbwR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/009iczKbwR",
+							"expanded_url": "http://www.theroadbook.co.uk",
+							"display_url": "theroadbook.co.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 117362,
+			"friends_count": 2844,
+			"listed_count": 1712,
+			"created_at": "Thu Sep 11 09:54:48 +0000 2008",
+			"favourites_count": 7165,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 42933,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1569992638580793346/jD8eeLwh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1569992638580793346/jD8eeLwh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16237253/1606075773",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 212,
+		"favorite_count": 3836,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Hello BBC Sport. Decide if you are a wing of the government or not? I realise that this may disqualify me from ever again commentating on the mountain biking on the Commonwealth Games, but it’s a hill (literally) I am prepared (not literally) to die on. Also, I was awful at it."
+	},
+	{
 		"created_at": "Thu Mar 02 19:14:54 +0000 2023",
 		"id": 1631266854584090600,
 		"id_str": "1631372526596005908",
