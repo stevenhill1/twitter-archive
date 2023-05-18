ccfcb2fe84b9a289b4781e806e9417b4228dde25
Steven <steven.hill1@gmail.com>
2023-05-18 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6193,
-	"created_at": "Fri Mar 24 01:10:12 +0000 2023",
+	"tweets": 6194,
+	"created_at": "Thu May 18 01:10:12 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_05.js
@@ -0,0 +1,117 @@
+Grailbird.data.tweets_2023_05 = 
+[
+	{
+		"created_at": "Wed May 17 20:52:45 +0000 2023",
+		"id": 1658858617250955300,
+		"id_str": "1658938628670726147",
+		"full_text": "Fancy coming to work with me? There are two Policy Adviser vacancies in the research team at @ResEngland working on either our exciting E3 funding for small, excellent research units or our flagship infrastructure fund: https://t.co/Fd4ZjpsJgs and  https://t.co/cxjMU6lSut",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ResEngland",
+					"name": "Research England",
+					"id": 823853384138637300,
+					"id_str": "823853384138637313",
+					"indices": [
+						93,
+						104
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Fd4ZjpsJgs",
+					"expanded_url": "https://www.careersportal.co.uk/UKRI-careers/jobs/policy-adviser-3219",
+					"display_url": "careersportal.co.uk/UKRI-careers/j…",
+					"indices": [
+						220,
+						243
+					]
+				},
+				{
+					"url": "https://t.co/cxjMU6lSut",
+					"expanded_url": "https://www.careersportal.co.uk/UKRI-careers/jobs/policy-adviser-3220",
+					"display_url": "careersportal.co.uk/UKRI-careers/j…",
+					"indices": [
+						249,
+						272
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 183342721,
+			"id_str": "183342721",
+			"name": "Dr Helena Mills",
+			"screen_name": "helenaCmills",
+			"location": "",
+			"description": "Head of Research Funding at @ResEngland @UKRI_News. One time historian, lover of jumpsuits. My views, no-one else's.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1031,
+			"friends_count": 1147,
+			"listed_count": 15,
+			"created_at": "Thu Aug 26 19:34:20 +0000 2010",
+			"favourites_count": 1770,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2394,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1518298424835297281/5ugaztyo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1518298424835297281/5ugaztyo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/183342721/1430128733",
+			"profile_link_color": "9C3369",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Fancy coming to work with me? There are two Policy Adviser vacancies in the research team at @ResEngland working on either our exciting E3 funding for small, excellent research units or our flagship infrastructure fund: https://t.co/Fd4ZjpsJgs and  https://t.co/cxjMU6lSut"
+	}
+]
