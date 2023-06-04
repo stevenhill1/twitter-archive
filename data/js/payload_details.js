8f2c37cd6fcb470fabee5433b1760b29fd351f73
Steven <steven.hill1@gmail.com>
2023-06-04 02:10:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6195,
-	"created_at": "Sat May 27 01:10:12 +0000 2023",
+	"tweets": 6196,
+	"created_at": "Sun Jun 04 01:10:12 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_06.js
@@ -0,0 +1,119 @@
+Grailbird.data.tweets_2023_06 = 
+[
+	{
+		"created_at": "Sat Jun 03 20:11:14 +0000 2023",
+		"id": 1665088773875462100,
+		"id_str": "1665088773875462144",
+		"full_text": "@martin_eve @PJDunleavy Just to confirm Martin is correct. UKRI OA policy does not apply to block grant funding from RE. We will be consulting on next REF policy later in the year.",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "martin_eve",
+					"name": "Martin Paul Eve / mpe@ravenation.club",
+					"id": 118747590,
+					"id_str": "118747590",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "PJDunleavy",
+					"name": "Patrick Dunleavy",
+					"id": 133385126,
+					"id_str": "133385126",
+					"indices": [
+						12,
+						23
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1665065585816871000,
+		"in_reply_to_status_id_str": "1665065585816870915",
+		"in_reply_to_user_id": 118747590,
+		"in_reply_to_user_id_str": "118747590",
+		"in_reply_to_screen_name": "martin_eve",
+		"user": {
+			"id": 7731752,
+			"id_str": "7731752",
+			"name": "Steven Hill",
+			"screen_name": "stevenhill",
+			"location": "Wiltshire, UK",
+			"description": "Director of Research at Research England, a council of UK Research and Innovation. Interested in research policy, science, evidence and innovation.",
+			"url": "https://t.co/pEfaOcJfMm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEfaOcJfMm",
+							"expanded_url": "http://stevenhill.org.uk/",
+							"display_url": "stevenhill.org.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5342,
+			"friends_count": 811,
+			"listed_count": 170,
+			"created_at": "Thu Jul 26 08:20:58 +0000 2007",
+			"favourites_count": 105,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6086,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/594767200/Steven_Hill_photo_normal.jpg",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@martin_eve @PJDunleavy Just to confirm Martin is correct. UKRI OA policy does not apply to block grant funding from RE. We will be consulting on next REF policy later in the year."
+	}
+]
